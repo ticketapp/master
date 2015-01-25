84510dff6f6417e50c15d96047fde147705c7d32
Loann Delgado <loann.delgado@gmail.com>
2015-01-25 19:01:48 +0100

    before try components

--- public/js/controllers/CreateEvent.js
+++ public/js/controllers/CreateEvent.js
@@ -1,5 +1,6 @@
 app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
-    $scope.event = [];
+    $scope.newEvent = [];
+    $scope.eventFb = false;
     $scope.GetEventByUrl = function(){
         $http.get('https://graph.facebook.com/v2.2/' + $scope.eventFbUrl + '/?' +  'access_token=CAACEdEose0cBAMG7RI6vyqtZCRkzkxTYvUQO0z8cjZAmkgGLmikKUy3XwfuARbl8IsZA5maVZBmRZAYOLtCO2jHFJ7UZChUhWoLimilUpbAOv5ZC85ZA0O2ZBV2tRFgNBh5Kd8ZB2ZBTXJMA8reUFRiCFC6ei6ZBvZCzQthRQwcYEFaTkBkvf6p0TU5PQFUMlUBi79xdVE669HvnGCxUdFTcxITI4zr3kryb4x18ZD').
             success(function(data, status, headers, config) {
@@ -29,7 +30,7 @@ app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
         $http.get('https://graph.facebook.com/v2.2/' + id + '/?' +  'access_token=CAACEdEose0cBAMG7RI6vyqtZCRkzkxTYvUQO0z8cjZAmkgGLmikKUy3XwfuARbl8IsZA5maVZBmRZAYOLtCO2jHFJ7UZChUhWoLimilUpbAOv5ZC85ZA0O2ZBV2tRFgNBh5Kd8ZB2ZBTXJMA8reUFRiCFC6ei6ZBvZCzQthRQwcYEFaTkBkvf6p0TU5PQFUMlUBi79xdVE669HvnGCxUdFTcxITI4zr3kryb4x18ZD').
             success(function(data, status, headers, config) {
                 console.log(data);
-                $scope.event = data;
+                $scope.newEvent = data;
                 $scope.content = data.description.replace(/(\n\n)/g, "<br/><br/></div><div class='column large-12'>");
                 $scope.content = $scope.content.replace(/(\n)/g, "<br/>");
                 scopeReady = true;
@@ -41,8 +42,7 @@ app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
             });
         $http.get('https://graph.facebook.com/v2.2/' + id + '/?fields=cover&access_token=CAACEdEose0cBAMG7RI6vyqtZCRkzkxTYvUQO0z8cjZAmkgGLmikKUy3XwfuARbl8IsZA5maVZBmRZAYOLtCO2jHFJ7UZChUhWoLimilUpbAOv5ZC85ZA0O2ZBV2tRFgNBh5Kd8ZB2ZBTXJMA8reUFRiCFC6ei6ZBvZCzQthRQwcYEFaTkBkvf6p0TU5PQFUMlUBi79xdVE669HvnGCxUdFTcxITI4zr3kryb4x18ZD').
             success(function(data, status, headers, config) {
-                $scope.event.cover = data.cover;
-                console.log($scope.event.cover);
+                $scope.newEvent.img = data.cover.source;
                 cover = true;
                 insert();
             }).
@@ -54,18 +54,28 @@ app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
             });
         function insert () {
             if (scopeReady == true && cover == true) {
-                //$scope.event.description.description = $scope.event.description.description.replace("↵↵", "</div><div class='column large-12' ");
                 console.log($scope.content);
-                document.getElementsByTagName('iframe')[0].contentDocument.getElementById('content').getElementsByTagName('div')[0].innerHTML = '<img class="width100p" src="' + $scope.event.cover.source + '"/>' + '<div class="columns large-12"><h2>' + $scope.event.name + '</h2></div>' + '<div class="columns large-12">' +  $scope.content + '</div>';
+                document.getElementsByTagName('iframe')[0].contentDocument.getElementById('content').getElementsByTagName('div')[0].innerHTML = '<img class="width100p" src="' + $scope.newEvent.img + '"/>' + '<div class="columns large-12"><h2>' + $scope.newEvent.name + '</h2></div>' + '<div class="columns large-12">' +  $scope.content + '</div>';
+                $scope.eventFb = true;
             } else {
                 insert();
             }
         }
     };
+    $scope.addImg = function () {
+      if ($scope.newEvent.img != null && $scope.eventFb != true) {
+          document.getElementsByTagName('iframe')[0].contentDocument.getElementById('content').getElementsByTagName('div')[0].innerHTML = '<img class="width100p" src="' + $scope.newEvent.img + '"/>' + '<div class="columns large-12"><h2>' + $scope.newEvent.name + '</h2></div>'
+      }
+    };
     $scope.clearContent = function () {
-        $scope.event.description = document.getElementsByTagName('iframe')[0].contentDocument.getElementById('content').innerHTML.replace(/contenteditable=\"true\"/g, "" );
-        $scope.event.description = $scope.event.description.replace("ng-init=\"event.description = 'ecrire ici'\"", "");
-        $scope.event.description = $scope.event.description.replace("ng-binding", "");
-        console.log($scope.event)
+        $scope.newEvent.description = document.getElementsByTagName('iframe')[0].contentDocument.getElementById('content').innerHTML.replace(/contenteditable=\"true\"/g, "" );
+        $scope.newEvent.description = $scope.newEvent.description.replace("ng-init=\"event.description = 'ecrire ici'\"", "");
+        $scope.newEvent.description = $scope.newEvent.description.replace("ng-binding", "");
+        console.log($scope.newEvent)
+    };
+
+    $scope.createNewEvent = function () {
+        $http.post('/admin/createEvent', {event : $scope.newEvent});
+        console.log($scope.newEvent)
     }
 }]);
