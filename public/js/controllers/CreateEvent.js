d61917333b989e66e868cf8206144885d736848d
Loann Delgado <loann.delgado@gmail.com>
2015-01-27 20:39:19 +0100

    regLink

--- public/js/controllers/CreateEvent.js
+++ public/js/controllers/CreateEvent.js
@@ -6,7 +6,7 @@ app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
     $scope.newEvent.tarifs = [];
     $scope.eventFb = false;
     $scope.GetEventByUrl = function(){
-        $http.get('https://graph.facebook.com/v2.2/' + $scope.eventFbUrl + '/?' +  'access_token=CAACEdEose0cBAIf806MaiRv06gj9zpZBqQSStcvykZCCMNdvT8ElN0i0mfq2v8gQk3nGXaFFtZCMOT1rmn837Bm31girsZAdJe7hzy5voRGoivWk5iX0W61rjsGECF3v2bvZAEWkFMUmjUVf0LvCHZCN4x4psgSii0eZAc1osQXmqZAhCwvdys16hxlfG6VwbkZA6F4vMezAChgVtmAUOPSqo5w9Ka5HyhekZD').
+        $http.get('https://graph.facebook.com/v2.2/' + $scope.eventFbUrl + '/?' +  'access_token=CAACEdEose0cBALtZBj9UeqIqhIZAAnst83rxAf6eYHzBGEFRb1dny3lh9ZAoVOcATwZBL5zZC08FKq90MdGP5ZCqmjBIfYydkwSiI7OPC0OwFgqFxxPxgu4RrUCGmSABS5LLQ0gZCMA25bBSXne1Pbsi10p9dFytP7BNayOGuMrVCpLNZB0mOGLUR8nLpn4VrktFmUBbBvH9H7ow3hmxCcslaEIT3c4wqfkZD').
             success(function(data, status, headers, config) {
                 console.log(data);
             }).
@@ -16,7 +16,7 @@ app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
             });
     };
     $scope.searchEvent = function(){
-        $http.get('https://graph.facebook.com/v2.2/search?q='+ $scope.eventFbName + '&limit=15&type=event&access_token=CAACEdEose0cBAIf806MaiRv06gj9zpZBqQSStcvykZCCMNdvT8ElN0i0mfq2v8gQk3nGXaFFtZCMOT1rmn837Bm31girsZAdJe7hzy5voRGoivWk5iX0W61rjsGECF3v2bvZAEWkFMUmjUVf0LvCHZCN4x4psgSii0eZAc1osQXmqZAhCwvdys16hxlfG6VwbkZA6F4vMezAChgVtmAUOPSqo5w9Ka5HyhekZD').
+        $http.get('https://graph.facebook.com/v2.2/search?q='+ $scope.eventFbName + '&limit=15&type=event&access_token=CAACEdEose0cBALtZBj9UeqIqhIZAAnst83rxAf6eYHzBGEFRb1dny3lh9ZAoVOcATwZBL5zZC08FKq90MdGP5ZCqmjBIfYydkwSiI7OPC0OwFgqFxxPxgu4RrUCGmSABS5LLQ0gZCMA25bBSXne1Pbsi10p9dFytP7BNayOGuMrVCpLNZB0mOGLUR8nLpn4VrktFmUBbBvH9H7ow3hmxCcslaEIT3c4wqfkZD').
             success(function(data, status, headers, config) {
                 console.log(data.data);
 
@@ -31,12 +31,26 @@ app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
     $scope.GetEventById = function(id){
         var scopeReady = false;
         var cover = false;
-        $http.get('https://graph.facebook.com/v2.2/' + id + '/?' +  'access_token=CAACEdEose0cBAIf806MaiRv06gj9zpZBqQSStcvykZCCMNdvT8ElN0i0mfq2v8gQk3nGXaFFtZCMOT1rmn837Bm31girsZAdJe7hzy5voRGoivWk5iX0W61rjsGECF3v2bvZAEWkFMUmjUVf0LvCHZCN4x4psgSii0eZAc1osQXmqZAhCwvdys16hxlfG6VwbkZA6F4vMezAChgVtmAUOPSqo5w9Ka5HyhekZD').
+        $http.get('https://graph.facebook.com/v2.2/' + id + '/?' +  'access_token=CAACEdEose0cBALtZBj9UeqIqhIZAAnst83rxAf6eYHzBGEFRb1dny3lh9ZAoVOcATwZBL5zZC08FKq90MdGP5ZCqmjBIfYydkwSiI7OPC0OwFgqFxxPxgu4RrUCGmSABS5LLQ0gZCMA25bBSXne1Pbsi10p9dFytP7BNayOGuMrVCpLNZB0mOGLUR8nLpn4VrktFmUBbBvH9H7ow3hmxCcslaEIT3c4wqfkZD').
             success(function(data, status, headers, config) {
                 console.log(data);
                 //$scope.newEvent = data;
-                $scope.content = data.description.replace(/(\n\n)/g, "<br/><br/></div><div class='column large-12'>");
-                $scope.content = $scope.content.replace(/(\n)/g, "<br/>");
+                var links = /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;;
+                $scope.content = data.description.replace(/(\n\n)/g, " <br/><br/></div><div class='column large-12'>");
+                $scope.content = $scope.content.replace(/(\n)/g, " <br/>");
+                if (matchedLinks = $scope.content.match(links)) {
+                    var m = matchedLinks;
+                    var unique = [];
+                    for (var ii = 0; ii < m.length; ii++) {
+                        var current = m[ii];
+                        if (unique.indexOf(current) < 0) unique.push(current);
+                    }
+                    console.log(unique);
+                    for (var i=0; i < unique.length; i++) {
+                        $scope.content = $scope.content.replace(new RegExp(unique[i],"g"),
+                                "<a href='" + unique[i]+ "'>" + unique[i] + "</a>")
+                    }
+                }
                 $scope.newEvent.name = data.name;
                 $scope.newEvent.place = data.location;
                 $scope.newEvent.startDate = new Date(data.start_time);
@@ -51,7 +65,7 @@ app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
             });
-        $http.get('https://graph.facebook.com/v2.2/' + id + '/?fields=cover&access_token=CAACEdEose0cBAIf806MaiRv06gj9zpZBqQSStcvykZCCMNdvT8ElN0i0mfq2v8gQk3nGXaFFtZCMOT1rmn837Bm31girsZAdJe7hzy5voRGoivWk5iX0W61rjsGECF3v2bvZAEWkFMUmjUVf0LvCHZCN4x4psgSii0eZAc1osQXmqZAhCwvdys16hxlfG6VwbkZA6F4vMezAChgVtmAUOPSqo5w9Ka5HyhekZD').
+        $http.get('https://graph.facebook.com/v2.2/' + id + '/?fields=cover&access_token=CAACEdEose0cBALtZBj9UeqIqhIZAAnst83rxAf6eYHzBGEFRb1dny3lh9ZAoVOcATwZBL5zZC08FKq90MdGP5ZCqmjBIfYydkwSiI7OPC0OwFgqFxxPxgu4RrUCGmSABS5LLQ0gZCMA25bBSXne1Pbsi10p9dFytP7BNayOGuMrVCpLNZB0mOGLUR8nLpn4VrktFmUBbBvH9H7ow3hmxCcslaEIT3c4wqfkZD').
             success(function(data, status, headers, config) {
                 $scope.newEvent.img = data.cover.source;
                 cover = true;
@@ -91,7 +105,7 @@ app.controller('CreateEventCtrl',['$scope', '$http', function($scope, $http){
         $scope.newEvent.endDate = $scope.newEvent.endDate.getFullYear() + '-' + $scope.newEvent.endDate.getMonth()+1 + '-' + $scope.newEvent.endDate.getDate();
         $scope.newEvent.startTime = $scope.newEvent.startTime.getFullYear() + '-' + $scope.newEvent.startTime.getMonth()+1 + '-' + $scope.newEvent.startTime.getDate();
         $scope.newEvent.endTime = $scope.newEvent.endTime.getFullYear() + '-' + $scope.newEvent.endTime.getMonth()+1 + '-' + $scope.newEvent.endTime.getDate();
-        console.log($scope.newEvent)
+
         $http.post('/createEvent', {
             name: $scope.newEvent.name,
             startSellingTime: $scope.newEvent.startDate,
