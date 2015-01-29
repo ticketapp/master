edfd6377972d65ba301bf5cd2d9d498d32dd62df
Loann Delgado <loann.delgado@gmail.com>
2015-01-29 02:32:57 +0100

    login

--- public/js/controllers/artistes.js
+++ public/js/controllers/artistes.js
@@ -1,5 +1,5 @@
 
-	app.controller ('ArtistesCtrl', function ($scope, ArtisteFactory, $routeParams ){
+	app.controller ('ArtistesCtrl', function ($scope, ArtisteFactory, $routeParams, $http ){
 	var artiste = ArtisteFactory.getArtiste($routeParams.id).then(function(artiste){
 	$scope.artiste = artiste;
 	}, function(msg){
