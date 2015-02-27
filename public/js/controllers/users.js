e0cece12a704631c692f86c0f41f70d1b3ab6981
Loann Delgado <loann.delgado@gmail.com>
2015-02-28 00:11:26 +0100

    resize user if 1 event

--- public/js/controllers/users.js
+++ public/js/controllers/users.js
@@ -25,18 +25,30 @@ app.controller ('UsersCtrl', function ($scope, UserFactory, $routeParams, $http,
                             console.log('yo')
                             document.getElementsByClassName('descriptionContent')[0].classList.remove('large-8');
                             document.getElementsByClassName('descriptionContent')[0].classList.add('large-4');
-                            document.getElementsByClassName('data-ng-event')[0].setAttribute('flex-gt-lg', '100');
+                            document.getElementsByClassName('descriptionContent')[0].classList.add('paddingLeft0');
+                            document.getElementsByClassName('data-ng-event')[0].classList.add('width100p');
+                            document.getElementsByClassName('min_contener')[0].classList.add('padding0');
+                            imgHeight();
+                            var descPlace = document.getElementsByClassName('descriptionContent')[0].getBoundingClientRect();
                             for (var i = 0; i < eventInfoConteners.length; i++) {
                                 eventInfoConteners[i].classList.remove('large-4');
                                 eventInfoConteners[i].classList.add('large-8');
+                                var infoPlace = eventInfoConteners[i].getBoundingClientRect();
+                                if (infoPlace.top > descPlace.bottom) {
+                                    eventInfoConteners[i].classList.remove('large-8');
+                                    eventInfoConteners[i].classList.add('large-12');
+                                }
                             }
                         }
+                        setTimeout(function(){
                         for (var i = 0; i < eventInfoConteners.length; i++) {
                             if (eventInfoConteners[i].offsetLeft < 30) {
                                 eventInfoConteners[i].classList.remove('large-4');
+                                eventInfoConteners[i].classList.remove('large-8');
                                 eventInfoConteners[i].classList.add('large-12');
                             }
                         }
+                        },100);
                     }
                 }, 100);
             } else if ( $rootScope.window != 'small' || $rootScope.window != 'medium') {
