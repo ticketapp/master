ab6a3c13238076aa2d45e83435e8a829225ba1dc
Loann Delgado <loann.delgado@gmail.com>
2015-03-07 23:19:02 +0100

    resize artist tracks

--- public/js/controllers/artistes.js
+++ public/js/controllers/artistes.js
@@ -3,6 +3,54 @@ app.controller ('ArtistesCtrl', function ($scope, ArtisteFactory, $routeParams, 
     $scope.trackLimit = 10;
     $scope.heightDesc = '147px';
     $scope.trackTitle = '';
+    function resizePageElem () {
+        var waitForBinding = setInterval(function () {
+            if (document.getElementById('events_contener').innerHTML.length > 0) {
+                clearInterval(waitForBinding);
+                function passElementTo100() {
+                    eventInfoConteners[i].classList.remove('large-4');
+                    eventInfoConteners[i].classList.remove('large-8');
+                    eventInfoConteners[i].classList.add('large-12');
+                    if (eventInfoConteners[i].className.indexOf('tracksContener') > -1) {
+                        $scope.bigTracks = false;
+                    }
+                }
+                var eventInfoConteners = document.getElementsByClassName('eventInfo');
+                /*if ($scope.orgaEvents.length == 1) {
+                 document.getElementsByClassName('descriptionContent')[0].classList.remove('large-8');
+                 document.getElementsByClassName('descriptionContent')[0].classList.add('large-4');
+                 document.getElementsByClassName('descriptionContent')[0].classList.add('paddingLeft0');
+                 document.getElementsByClassName('data-ng-event')[0].classList.add('width100p');
+                 document.getElementsByClassName('min_contener')[0].classList.add('padding0');
+                 $rootScope.resizeImgHeight();
+                 var descPlace = document.getElementsByClassName('descriptionContent')[0].getBoundingClientRect();
+                 for (var i = 0; i < eventInfoConteners.length; i++) {
+                 eventInfoConteners[i].classList.remove('large-4');
+                 eventInfoConteners[i].classList.add('large-8');
+                 if (eventInfoConteners[i].offsetLeft < 30) {
+                 passElementTo100()
+                 }
+                 }
+                 }
+                 } else {*/
+                    for (var i = 0; i < eventInfoConteners.length; i++) {
+                        passElementTo100()
+                    }
+                //}
+            }
+        }, 100);
+    }
+    function watchWindowSize () {
+        if ($rootScope.window != 'small' && $rootScope.window != 'medium') {
+            resizePageElem()
+        } else {
+            $rootScope.$watch('window', function (newval) {
+                if (newval == 'large' || newval == 'xlarge' || newval == 'xxlarge') {
+                    resizePageElem()
+                }
+            })
+        }
+    }
     function refactorWebsites () {
         for (var i = 0; i < $scope.artiste.websites.length; i++) {
             $scope.artiste.websites[i] = {url: $scope.artiste.websites[i]};
@@ -21,86 +69,17 @@ app.controller ('ArtistesCtrl', function ($scope, ArtisteFactory, $routeParams, 
         }
     }
     if ($rootScope.artisteToCreate == false) {
-        console.log('not creat')
         $http.get('/artists/' + $routeParams.facebookUrl)
             .success(function (data, status) {
                 $scope.artiste = data;
                 refactorWebsites();
+                watchWindowSize();
             }).error(function (data, status) {
             });
     } else {
         refactorWebsites();
         $rootScope.passArtisteToCreateToFalse();
-        console.log($scope.artiste)
+        watchWindowSize();
     }
-    /*$http.get('/artists/' + $routeParams.id +'/events')
-     .success(function(data, status){
-     $scope.orgaEvents = data;*/
     $rootScope.resizeImgHeight();
-    //console.log($scope.orgaEvents);
-    if (/*$scope.orgaEvents.length > 0 &&*/ $rootScope.window != 'small' && $rootScope.window != 'medium') {
-        var waitForBinding = setInterval(function () {
-            if (document.getElementById('events_contener').innerHTML.length > 0) {
-                clearInterval(waitForBinding);
-                var eventInfoConteners = document.getElementsByClassName('eventInfo');
-                /*if ($scope.orgaEvents.length == 1) {
-                 document.getElementsByClassName('descriptionContent')[0].classList.remove('large-8');
-                 document.getElementsByClassName('descriptionContent')[0].classList.add('large-4');
-                 document.getElementsByClassName('descriptionContent')[0].classList.add('paddingLeft0');
-                 document.getElementsByClassName('data-ng-event')[0].classList.add('width100p');
-                 document.getElementsByClassName('min_contener')[0].classList.add('padding0');
-                 $rootScope.resizeImgHeight();
-                 var descPlace = document.getElementsByClassName('descriptionContent')[0].getBoundingClientRect();
-                 for (var i = 0; i < eventInfoConteners.length; i++) {
-                 eventInfoConteners[i].classList.remove('large-4');
-                 eventInfoConteners[i].classList.add('large-8');
-                 var infoPlace = eventInfoConteners[i].getBoundingClientRect();
-                 if (infoPlace.top > descPlace.bottom) {
-                 eventInfoConteners[i].classList.remove('large-8');
-                 eventInfoConteners[i].classList.add('large-12');
-                 }
-                 }
-                 }*/
-                angular.element(document).ready(function () {
-                    for (var i = 0; i < eventInfoConteners.length; i++) {
-                        if (eventInfoConteners[i].offsetLeft < 30) {
-                            eventInfoConteners[i].classList.remove('large-4');
-                            eventInfoConteners[i].classList.remove('large-8');
-                            eventInfoConteners[i].classList.add('large-12');
-                            if (eventInfoConteners[i].className.indexOf('tracksContener') > -1) {
-                                $scope.bigTracks = false;
-                            }
-                        }
-                    }
-                })
-            }
-        }, 100);
-    } else {
-        $rootScope.$watch('window', function(newval) {
-            if (newval == 'large' || newval == 'xlarge' || newval == 'xxlarge') {
-                var waitForBinding = setInterval(function () {
-                    if (document.getElementById('events_contener').innerHTML.length > 0) {
-                        clearInterval(waitForBinding);
-                        var eventInfoConteners = document.getElementsByClassName('eventInfo');
-                        for (var i = 0; i < eventInfoConteners.length; i++) {
-                            if (eventInfoConteners[i].offsetLeft < 30) {
-                                eventInfoConteners[i].classList.remove('large-4');
-                                eventInfoConteners[i].classList.add('large-12');
-                            }
-                        }
-                    } else {
-                        clearInterval(waitForBinding);
-                        var eventInfoConteners = document.getElementsByClassName('eventInfo');
-                        for (var i = 0; i < eventInfoConteners.length; i++) {
-                            if (eventInfoConteners[i].offsetLeft < 30) {
-                                eventInfoConteners[i].classList.remove('large-4');
-                                eventInfoConteners[i].classList.add('large-12');
-                            }
-                        }
-                    }
-                }, 100);
-            }
-        })
-    }
-    //});
 });
