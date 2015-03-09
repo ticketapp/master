dbaf779768a6933d4288c251bdcabcc3a30195d9
Loann Delgado <loann.delgado@gmail.com>
2015-03-09 04:24:49 +0100

    map resize user

--- public/js/controllers/users.js
+++ public/js/controllers/users.js
@@ -1,6 +1,7 @@
 app.controller ('UsersCtrl', function ($scope, UserFactory, $routeParams, $http, $rootScope){
     $scope.map = false;
     $scope.heightMap = 300;
+    console.log($routeParams);
     $http.get('/organizers/' + $routeParams.id +'/events')
         .success(function(data, status){
             $scope.orgaEvents = data;
@@ -13,13 +14,58 @@ app.controller ('UsersCtrl', function ($scope, UserFactory, $routeParams, $http,
                         var eventInfoConteners = document.getElementsByClassName('eventInfo');
                         var eventsContener = document.getElementsByClassName('data-ng-event');
                         var indexEventRef =(Math.ceil(
-                            eventInfoConteners[0].clientHeight / eventsContener[0].clientHeight)*2)-1;
-                        if (indexEventRef == $scope.orgaEvents.length -1) {
+                            eventInfoConteners[0].clientHeight / eventsContener[0].clientHeight)*2);
+                        if (indexEventRef >= $scope.orgaEvents.length -1) {
                             $scope.largeMap = true;
-                        } else {
-                            var lastEventPos = eventsContener[indexEventRef].getBoundingClientRect();
+                            $scope.$apply();
+                            eventInfoConteners = document.getElementsByClassName('eventInfo');
+                            if (indexEventRef -1 < eventsContener.length -1 && indexEventRef -1 > 0) {
+                                var lastEventPos = eventsContener[indexEventRef -1].getBoundingClientRect();
+                                console.log(lastEventPos)
+                            } else {
+                                var lastEventPos = eventsContener[eventsContener.length - 1].getBoundingClientRect();
+                                console.log(lastEventPos)
+                            }
                             var descPos = eventInfoConteners[0].getBoundingClientRect();
+                            console.log(descPos)
                             var newHeight = (lastEventPos.bottom - descPos.bottom) - 10;
+                            console.log(newHeight)
+                            if (newHeight < (eventsContener[0].clientHeight)/3) {
+                                if (newHeight > 0) {
+                                    eventInfoConteners[0].style.paddingBottom = newHeight + 'px';
+                                }
+                                console.log(newHeight)
+                                console.log(eventsContener[0].clientHeight/3);
+                                if (newHeight < -(eventsContener[0].clientHeight/3)) {
+                                    $scope.medMap = true;
+                                } else if (newHeight < (eventsContener[0].clientHeight)/3) {
+                                    console.log(eventsContener.length)
+                                    if (eventsContener.length == 1 || eventsContener.length %2 == 0) {
+                                        $scope.fullMap = true;
+                                    }
+                                } else {
+                                    descPos = eventInfoConteners[1].getBoundingClientRect();
+                                    lastEventPos = eventsContener[eventsContener.length -1].getBoundingClientRect();
+                                    newHeight = (lastEventPos.bottom - descPos.bottom) - 10;
+                                    $scope.heightMap = newHeight + 'px';
+                                }
+                            } else {
+                                descPos = eventInfoConteners[1].getBoundingClientRect();
+                                lastEventPos = eventsContener[eventsContener.length -1].getBoundingClientRect();
+                                newHeight = (lastEventPos.bottom - descPos.bottom) - 10;
+                             $scope.heightMap = newHeight + 'px';
+                            }
+                        } else {
+                            $scope.largeMap = false;
+                            var lastEventPos;
+                            if (indexEventRef <= 0) {
+                                lastEventPos = eventsContener[0].getBoundingClientRect()
+                            } else {
+                                lastEventPos = eventsContener[indexEventRef -1].getBoundingClientRect();
+                            }
+                            console.log(indexEventRef)
+                            var descPos = eventInfoConteners[0].getBoundingClientRect();
+                            var newHeight = (lastEventPos.bottom - descPos.bottom) - 9;
                             console.log(newHeight)
                             console.log(eventsContener[0].clientHeight/3)
                             if (newHeight < (eventsContener[0].clientHeight)/3) {
@@ -29,12 +75,15 @@ app.controller ('UsersCtrl', function ($scope, UserFactory, $routeParams, $http,
                             }
                             $scope.heightMap = newHeight + 'px';
                             //$scope.heightMap = lastEventPos.bottom - descPos.bottom;
-                            $scope.map = true;
-                            $scope.$apply();
                         }
-                    }, 1000)
+                        $scope.map = true;
+                        $scope.$apply();
+                    }, 500)
                 })
             } else {
+                $scope.map = true;
+                $scope.largeMap = true;
+                $scope.fullMap = true;
                 $rootScope.$watch('window', function(newval) {
                     if (newval == 'large' || newval == 'xlarge' || newval == 'xxlarge') {
                         var waitForBinding = setInterval(function () {
