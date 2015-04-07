b4c730dfa8b63828a125f86e06d0532a1575d67b
simon <simongarnier07@hotmail.fr>
2015-04-07 20:20:55 +0200

    login redirects to #/connected

--- app/controllers/PlaylistController.scala
+++ app/controllers/PlaylistController.scala
@@ -7,7 +7,6 @@ import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import securesocial.core._
 import play.api.libs.concurrent.Execution.Implicits._
-
 import scala.concurrent.Future
 
 object PlaylistController extends Controller with securesocial.core.SecureSocial {
