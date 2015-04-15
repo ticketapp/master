8e3a099090fe6a6745e9d7bd732defa804270111
Loann Delgado <loann.delgado@gmail.com>
2015-04-15 05:27:02 +0200

    all min

--- public/js/controllers/iframeCtrl.js
+++ public/js/controllers/iframeCtrl.js
@@ -11,17 +11,25 @@ app.controller('iframeCtrl', function ($http, $scope, $rootScope){
                     scopeIdList.push(el.eventId);
                 }
                 $scope.events.forEach(getEventId);
-                if ($scope.events.length == 0) {
-                    $scope.events = data;
-                } else {
                     function uploadEvents(el, index, array) {
                         if (scopeIdList.indexOf(el.eventId) == -1) {
+                            el.priceColor = 'rgb(0, 140, 186)';
+                            if (el.tariffRange != undefined) {
+                                var tariffs = el.tariffRange.split('-');
+                                if (tariffs[1] > tariffs[0]) {
+                                    el.tariffRange = tariffs[0].replace('.0', '') + '€ - ' +
+                                        tariffs[1].replace('.0', '') + '€';
+                                } else {
+                                    el.tariffRange = tariffs[0].replace('.0', '') + '€';
+                                }
+                                el.priceColor = 'rgb(' + tariffs[0]*10 + ',' + (250 - (tariffs[0]*10 ) )+
+                                    ',' + (175 - (tariffs[0]*10 )) + ')'
+                            }
                             $scope.events.push(el);
                         }
                     }
 
                     data.forEach(uploadEvents)
-                }
                 console.log($scope.events)
                 $rootScope.resizeImgHeight();
             }).
