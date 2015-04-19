abb9f8b404bfd74b6b2f6d326a5afa8340e1a4da
Loann Delgado <loann.delgado@gmail.com>
2015-04-19 17:30:54 +0200

    initMapOk

--- public/js/controllers/phoneHomeCtrl.js
+++ public/js/controllers/phoneHomeCtrl.js
@@ -9,7 +9,8 @@ app.controller('phoneHomeCtrl', function ($scope, $rootScope, $http, $timeout, $
     var time = 6;
     var offset = 0;
     var map;
-    function getEvents () {
+    var firstShow = true;
+    $scope.getEvents = function () {
         $http.get('/events/inInterval/' + time + '/(' + $scope.mapCenter.replace(/^.+,/,'') + ',' +
             $scope.mapCenter.substring(0, $scope.mapCenter.indexOf(',')) + ')/20/'+ offset).
             success(function (data, status, headers, config) {
@@ -25,26 +26,16 @@ app.controller('phoneHomeCtrl', function ($scope, $rootScope, $http, $timeout, $
                             geoPoint.substring(0, geoPoint.indexOf(',')) >= $scope.mapBounces.Da.k &&
                             geoPoint.replace(/^.+,/,'') <= $scope.mapBounces.va.k &&
                             geoPoint.replace(/^.+,/,'') >= $scope.mapBounces.va.j) {
-                            console.log('youpi')
                             eventInBounce = true;
                         }
                     }
-                    if (i == $scope.events.length -1) {
-                        if (eventInBounce == false) {
+                    if (i == $scope.events.length -1 ) {
+                        if (eventInBounce == false && firstShow == true) {
                             $scope.time = $scope.time + 1;
-                            if ($scope.time > 23 && $scope.time <= 38) {
-                                $scope.selectedTime = ($scope.time-23)*24
-                            } else if ($scope.time > 38 && $scope.time <= 40) {
-                                $scope.selectedTime = ($scope.time-36)*168;
-                            } else if ($scope.time > 40) {
-                                $scope.selectedTime = ($scope.time-39)*720;
-                            } else {
-                                $scope.selectedTime = $scope.time;
-                            }
-                            time = $scope.selectedTime;
-                            getEvents()
+                            $scope.timeChange();
 
                         } else {
+                            firstShow = false;
                             $scope.updateMarkers()
                         }
                     }
@@ -71,9 +62,113 @@ app.controller('phoneHomeCtrl', function ($scope, $rootScope, $http, $timeout, $
         }, 100);
     }
     $scope.initializeTime();
+    $scope.updateMarkers = function () {
+        $scope.dynMarkers = [];
+        if ($scope.markerClusterer != undefined) {
+            $scope.markerClusterer.clearMarkers();
+        }
+        var eventsLength = $scope.events.length;
+        for (var i=0; i<eventsLength; i++) {
+            if ($scope.events[i].addresses[0] != undefined) {
+                var geoPoint = $scope.events[i].addresses[0].geographicPoint;
+                var markerGenre;
+                function pushMarker (markerGenre, geoPoint) {
+                    var latLng = new google.maps.LatLng(geoPoint.substring(0, geoPoint.indexOf(',')), geoPoint.replace(/^.+,/,''));
+                    $scope.dynMarkers.push(new google.maps.Marker({position: latLng, icon: '../assets/img/' + markerGenre}));
+                }
+                if ($scope.events[i].genres.length == 0) {
+                    markerGenre = 'music187.png';
+                } else {
+                    var foundMarkerGenre = false;
+                    for (var g = 0; g < $scope.events[i].genres.length; g++) {
+                        console.log($scope.events[i].genres[g])
+                        if ($scope.events[i].genres[g].name == 'electro' ||
+                            $scope.events[i].genres[g].name == 'techno' ||
+                            $scope.events[i].genres[g].name == 'd&b' ||
+                            $scope.events[i].genres[g].name == 'drum' ||
+                            $scope.events[i].genres[g].name == 'dubstep' ||
+                            $scope.events[i].genres[g].name == 'trance' && foundMarkerGenre == false) {
+                            markerGenre = 'music200.png';
+                            foundMarkerGenre = true;
+                        } else if ($scope.events[i].genres[g].name.indexOf('rock') > -1 && foundMarkerGenre == false) {
+                            markerGenre = 'rock3.png';
+                            foundMarkerGenre = true;
+                        } else if ($scope.events[i].genres[g].name.indexOf('reggae') > -1 ||
+                            $scope.events[i].genres[g].name == 'dub' && foundMarkerGenre == false) {
+                            markerGenre = 'jamaica_640.png';
+                            foundMarkerGenre = true;
+                        }
+                        console.log(markerGenre)
+                    }
+                    if (foundMarkerGenre == false) {
+                        markerGenre = 'music187.png';
+                    }
+                }
+                pushMarker(markerGenre, geoPoint);
+            }
+        }
+        $scope.markerClusterer = new MarkerClusterer(map, $scope.dynMarkers, {});
+        $scope.markerClusterer.zoomOnClick_ = false;
+        var markersLength = $scope.markerClusterer.markers_.length
+        for (i = 0; i < markersLength; i++) {
+            var marker = $scope.markerClusterer.markers_[i]
+            google.maps.event.addListener(marker, 'click', function(marker) {
+                var redirectPath = ''
+                var eventsLength = $scope.events.length;
+                for (var i = 0; i < eventsLength; i++) {
+                    if ($scope.events[i].addresses[0] != undefined) {
+                        var geopoint = $scope.events[i].addresses[0].geographicPoint
+                        console.log(geopoint)
+                        if (geopoint.substring(0, geopoint.indexOf(',')) < marker.latLng.k + 0.000001 &&
+                            geopoint.substring(0, geopoint.indexOf(',')) > marker.latLng.k - 0.000001 &&
+                            geopoint.replace(/^.+,/,'') < marker.latLng.D + 0.000001 &&
+                            geopoint.replace(/^.+,/,'') > marker.latLng.D - 0.000001) {
+                            redirectPath = 'event/' + $scope.events[i].eventId;
+                        }
+                    }
+                }
+                window.location.href =('#/' + redirectPath);
+            });
+        }
+        google.maps.event.addListener($scope.markerClusterer, 'clusterclick', function(cluster) {
+            console.log(cluster)
+            var eventsLength = $scope.events.length;
+            var redirectPath = '';
+            var count =0;
+            var places = '';
+            for (var i = 0; i < eventsLength; i++) {
+                if ($scope.events[i].addresses[0] != undefined) {
+                    var geopoints = $scope.events[i].addresses[0].geographicPoint
+                    var markerLength = cluster.markers_.length;
+                    for (var m = 0; m < markerLength; m++) {
+                        if (geopoints.substring(0, geopoints.indexOf(',')) < cluster.markers_[m].position.k + 0.000001 &&
+                            geopoints.substring(0, geopoints.indexOf(',')) > cluster.markers_[m].position.k - 0.000001 &&
+                            geopoints.replace(/^.+,/, '') < cluster.markers_[m].position.D + 0.000001 &&
+                            geopoints.replace(/^.+,/, '') > cluster.markers_[m].position.D - 0.000001) {
+                            if (places.indexOf($scope.events[i].places[0].placeId) == -1) {
+                                places = places + ', ' + $scope.events[i].places[0].placeId;
+                                count = count + 1;
+                            }
+                        }
+                    }
+                }
+            }
+            if (count == 1) {
+                redirectPath = 'lieu/' + places.replace(', ', '');
+            } else {
+                $timeout(function () {
+                    $scope.$apply(function () {
+                        $scope.mapCenter = cluster.center_.k + ',' + cluster.center_.D;
+                        $scope.zoom = 14;
+                    })
+                }, 0)
+            }
+            window.location.href =('#/' + redirectPath);
+        });
+    };
     var StartTimer;
     var doneStartInterval = 600;
-    $scope.updateMarkers = function () {
+    $scope.timeChange = function () {
         if ($scope.time > 23 && $scope.time <= 38) {
             $scope.selectedTime = ($scope.time-23)*24
         } else if ($scope.time > 38 && $scope.time <= 40) {
@@ -83,121 +178,16 @@ app.controller('phoneHomeCtrl', function ($scope, $rootScope, $http, $timeout, $
         } else {
             $scope.selectedTime = $scope.time;
         }
+        time = $scope.selectedTime;
         var textSlider = document.getElementsByClassName('md-thumb');
         for (var i = 0; i < textSlider.length; i++) {
             textSlider[i].innerHTML = '';
             textSlider[i].innerHTML = textSlider[i].innerHTML + '<b style="color: #ffffff">' +
                 $filter('millSecondsToTimeString')($scope.selectedTime) + '</b>';
         }
-        StartTimer = setTimeout(function () {
-            $scope.dynMarkers = [];
-            if ($scope.markerClusterer != undefined) {
-                $scope.markerClusterer.clearMarkers();
-            }
-            var eventsLength = $scope.events.length;
-            for (var i=0; i<eventsLength; i++) {
-                if ($scope.events[i].addresses[0] != undefined) {
-                    var geoPoint = $scope.events[i].addresses[0].geographicPoint;
-                    var markerGenre;
-                    function pushMarker (markerGenre, geoPoint) {
-                        var latLng = new google.maps.LatLng(geoPoint.substring(0, geoPoint.indexOf(',')), geoPoint.replace(/^.+,/,''));
-                        $scope.dynMarkers.push(new google.maps.Marker({position: latLng, icon: '../assets/img/' + markerGenre}));
-                    }
-                    if ($scope.events[i].genres.length == 0) {
-                        markerGenre = 'music187.png';
-                    } else {
-                        var foundMarkerGenre = false;
-                        for (var g = 0; g < $scope.events[i].genres.length; g++) {
-                            console.log($scope.events[i].genres[g])
-                            if ($scope.events[i].genres[g].name == 'electro' ||
-                                $scope.events[i].genres[g].name == 'techno' ||
-                                $scope.events[i].genres[g].name == 'd&b' ||
-                                $scope.events[i].genres[g].name == 'drum' ||
-                                $scope.events[i].genres[g].name == 'dubstep' ||
-                                $scope.events[i].genres[g].name == 'trance' && foundMarkerGenre == false) {
-                                markerGenre = 'music200.png';
-                                foundMarkerGenre = true;
-                            } else if ($scope.events[i].genres[g].name.indexOf('rock') > -1 && foundMarkerGenre == false) {
-                                markerGenre = 'rock3.png';
-                                foundMarkerGenre = true;
-                            } else if ($scope.events[i].genres[g].name.indexOf('reggae') > -1 ||
-                                $scope.events[i].genres[g].name == 'dub' && foundMarkerGenre == false) {
-                                markerGenre = 'jamaica_640.png';
-                                foundMarkerGenre = true;
-                            }
-                            console.log(markerGenre)
-                        }
-                        if (foundMarkerGenre == false) {
-                            markerGenre = 'music187.png';
-                        }
-                    }
-                    pushMarker(markerGenre, geoPoint);
-                }
-            }
-            $scope.markerClusterer = new MarkerClusterer(map, $scope.dynMarkers, {});
-            $scope.markerClusterer.zoomOnClick_ = false;
-            var markersLength = $scope.markerClusterer.markers_.length
-            for (i = 0; i < markersLength; i++) {
-                var marker = $scope.markerClusterer.markers_[i]
-                google.maps.event.addListener(marker, 'click', function(marker) {
-                    var redirectPath = ''
-                    var eventsLength = $scope.events.length;
-                    for (var i = 0; i < eventsLength; i++) {
-                        if ($scope.events[i].addresses[0] != undefined) {
-                            var geopoint = $scope.events[i].addresses[0].geographicPoint
-                            console.log(geopoint)
-                            if (geopoint.substring(0, geopoint.indexOf(',')) < marker.latLng.k + 0.000001 &&
-                                geopoint.substring(0, geopoint.indexOf(',')) > marker.latLng.k - 0.000001 &&
-                                geopoint.replace(/^.+,/,'') < marker.latLng.D + 0.000001 &&
-                                geopoint.replace(/^.+,/,'') > marker.latLng.D - 0.000001) {
-                                redirectPath = 'event/' + $scope.events[i].eventId;
-                            }
-                        }
-                    }
-                    window.location.href =('#/' + redirectPath);
-                });
-            }
-            google.maps.event.addListener($scope.markerClusterer, 'clusterclick', function(cluster) {
-                console.log(cluster)
-                var eventsLength = $scope.events.length;
-                var redirectPath = '';
-                var count =0;
-                var places = '';
-                for (var i = 0; i < eventsLength; i++) {
-                    if ($scope.events[i].addresses[0] != undefined) {
-                        var geopoints = $scope.events[i].addresses[0].geographicPoint
-                        var markerLength = cluster.markers_.length;
-                        for (var m = 0; m < markerLength; m++) {
-                            if (geopoints.substring(0, geopoints.indexOf(',')) < cluster.markers_[m].position.k + 0.000001 &&
-                                geopoints.substring(0, geopoints.indexOf(',')) > cluster.markers_[m].position.k - 0.000001 &&
-                                geopoints.replace(/^.+,/, '') < cluster.markers_[m].position.D + 0.000001 &&
-                                geopoints.replace(/^.+,/, '') > cluster.markers_[m].position.D - 0.000001) {
-                                if (places.indexOf($scope.events[i].places[0].placeId) == -1) {
-                                    places = places + ', ' + $scope.events[i].places[0].placeId;
-                                    count = count + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-                if (count == 1) {
-                    redirectPath = 'lieu/' + places.replace(', ', '');
-                } else {
-                    $timeout(function () {
-                        $scope.$apply(function () {
-                            $scope.mapCenter = cluster.center_.k + ',' + cluster.center_.D;
-                            $scope.zoom = 14;
-                        })
-                    }, 0)
-                }
-                window.location.href =('#/' + redirectPath);
-            })
-            if ($scope.selectedTime > time) {
-                time = $scope.selectedTime;
-                getEvents()
-            }
-        }, doneStartInterval);
-    };
+        clearTimeout(StartTimer)
+        StartTimer = setTimeout($scope.getEvents, doneStartInterval);
+    }
     $scope.goTo = function (e, id) {
         var redirectPath = 'event/' + $scope.events[id].eventId;
         var eventsLength = $scope.events.length;
@@ -226,10 +216,11 @@ app.controller('phoneHomeCtrl', function ($scope, $rootScope, $http, $timeout, $
                     }
                 });
                 google.maps.event.addListener(map, 'center_changed', function() {
-                    console.log('yo')
+                    $scope.mapBounces = map.getBounds()
+                    $scope.mapCenter = map.center;
                 })
                 $scope.mapBounces = map.getBounds()
-                getEvents()
+                $scope.getEvents()
             }
         }, 500)
     }
