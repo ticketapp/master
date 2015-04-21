160ab41687f4e9e00aab5f45ecb4e7e7c4178c73
Loann Delgado <loann.delgado@gmail.com>
2015-04-21 08:00:04 +0200

    bug editor

--- public/js/controllers/iframeCtrl.js
+++ public/js/controllers/iframeCtrl.js
@@ -2,147 +2,143 @@ app.controller('iframeCtrl', function ($http, $scope, $rootScope, $timeout, $fil
     var offset =  0;
     $scope.search = '';
     $scope.events = [];
-    console.log($rootScope.geoLoc);
-    $scope.getEvents = function () {
-        if ($scope.search.length == 0) {
-            $http.get('/events/nearGeoPoint/' + $rootScope.geoLoc + '/12/' + offset).
-                success(function (data, status, headers, config) {
-                    var scopeIdList = [];
-
-                    function getEventId(el, index, array) {
-                        scopeIdList.push(el.eventId);
-                    }
-
-                    $scope.events.forEach(getEventId);
-                    function uploadEvents(el, index, array) {
-                        if (scopeIdList.indexOf(el.eventId) == -1) {
-                            el.priceColor = 'rgb(0, 140, 186)';
-                            if (el.tariffRange != undefined) {
-                                var tariffs = el.tariffRange.split('-');
-                                if (tariffs[1] > tariffs[0]) {
-                                    el.tariffRange = tariffs[0].replace('.0', '') + '€ - ' +
-                                        tariffs[1].replace('.0', '') + '€';
-                                } else {
-                                    el.tariffRange = tariffs[0].replace('.0', '') + '€';
-                                }
-                                el.priceColor = 'rgb(' + tariffs[0] * 10 + ',' + (250 - (tariffs[0] * 10 ) ) +
-                                    ',' + (175 - (tariffs[0] * 10 )) + ')'
-                            }
-                            $scope.events.push(el);
-                        }
-                    }
-
-                    data.forEach(uploadEvents)
-                    console.log($scope.events)
-                    $rootScope.resizeImgHeight();
-                }).
-                error(function (data, status, headers, config) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                });
-        } else {
-            $timeout(function() {
-                $scope.$apply(function () {
-                    $scope.events = $filter('filter')($scope.events, {name: $scope.search})
-                    $scope.scopeIdList = [];
-                })
-            },0);
-            $http.get('/events/containing/' + $scope.search + '/' + $rootScope.geoLoc).
-                success(function (data, status, headers, config) {
-                    function uploadEvents(el, index, array) {
-                        $timeout(function() {
-                            $scope.$apply(function () {
-                                function getEventsId(el, index, array) {
-                                    $scope.scopeIdList.push(el.eventId);
-                                }
-                                $scope.events.forEach(getEventsId);
-                            });
-                            if ($scope.scopeIdList.indexOf(el.eventId) == -1) {
-                                el.priceColor = 'rgb(0, 140, 186)';
-                                var placeLenght = el.places.length
-                                for (var i = 0; i < placeLenght; i++) {
-                                    if (el.places[i].geographicPoint != undefined) {
-                                        el.places[i].geographicPoint = el.geographicPoint.replace("(", "");
-                                        el.places[i].geographicPoint = el.geographicPoint.replace(")", "");
-                                        el.places[i].geographicPoint = el.geographicPoint.replace(",", ", ");
-                                    }
-                                }
-                                if (el.tariffRange != undefined) {
-                                    var tariffs = el.tariffRange.split('-');
-                                    if (tariffs[1] > tariffs[0]) {
-                                        el.tariffRange = tariffs[0].replace('.0', '') + '€ - ' +
-                                            tariffs[1].replace('.0', '') + '€';
-                                    } else {
-                                        el.tariffRange = tariffs[0].replace('.0', '') + '€';
-                                    }
-                                    el.priceColor = 'rgb(' + tariffs[0]*2 + ',' + (200 - (tariffs[0]*4 ) )+
-                                        ',' + tariffs[0]*4 + ')'
-                                }
-                                $scope.events.push(el);
-                                $scope.scopeIdList.push(el.eventId);
-                                console.log($scope.scopeIdList)
-
-                            }
-                        },0);
-                    }
-                    for (var i = 0; i < data.length; i++) {
-                        if (data[i].name.toLowerCase().indexOf($scope.search.toLowerCase()) > -1) {
-                            uploadEvents(data[i]);
-                        }
-                    }
-                    $scope.loadingMore = false;
-                    $http.get('/artists/containing/'+$scope.search).
-                        success(function(data, status, headers, config) {
-                            function getArtistEvents (art) {
-                                $http.get('/artists/'+ art.facebookUrl + '/events ').
-                                    success(function(data){
-                                        data.forEach(uploadEvents);
-                                        $rootScope.resizeImgHeight()
-                                    })
-                            }
-                            data.forEach(getArtistEvents)
-                            $scope.loadingMore = false;
-                        });
-                    $http.get('/genres/'+ $scope.search +'/20/' + offset + '/events ').
-                        success(function(data, status, headers, config) {
-                            data.forEach(uploadEvents);
-                            $rootScope.resizeImgHeight()
-                        });
-                    $http.get('/places/containing/'+$scope.search).
-                        success(function(data, status, headers, config) {
-                            function getPlaceEvents (place) {
-                                $http.get('/places/'+ place.placeId + '/events ').
-                                    success(function(data){
-
-                                        data.forEach(uploadEvents);
-                                        $rootScope.resizeImgHeight()
-                                    })
-                            }
-                            data.forEach(getPlaceEvents)
-                        });
-                    $http.get('/events/nearCity/' + $scope.search + '/12/' + offset ).
-                        success(function (data) {
-
-                            data.forEach(uploadEvents);
-                            $rootScope.resizeImgHeight()
-                        });
-                    $rootScope.resizeImgHeight()
-                    $scope.loadingMore = false;
-                }).
-                error(function (data, status, headers, config) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                });
+    function updateScope (data, scope, idName, otherScopeToCheck) {
+        var scopeIdList = [];
+        function getId(el, index, array) {
+            var idDictionnary = {'artistId': el.artistId, 'eventId': el.eventId, 'organizerId': el.organizerId,
+                'placeId': el.placeId, 'facebookId': el.facebookId};
+            scopeIdList.push(idDictionnary[idName]);
+        }
+        if (otherScopeToCheck != undefined) {
+            otherScopeToCheck.forEach(getId);
+        }
+        scope.forEach(getId);
+        function pushEl (el, index, array) {
+            var idDictionnary = {'artistId': el.artistId, 'eventId': el.eventId, 'organizerId': el.organizerId,
+                'placeId': el.placeId, 'facebookId': el.facebookId};
+            if (scopeIdList.indexOf(idDictionnary[idName]) == -1) {
+                $timeout(function () {
+                    $scope.$apply(function () {
+                        scope.push(el);
+                    })
+                }, 0);
+            }
+        }
+        data.forEach(pushEl);
+        $rootScope.resizeImgHeight();
+        $scope.loadingMore = false;
+    }
+    function colorEvent(el) {
+        el.priceColor = 'rgb(0, 140, 186)';
+        if (el.tariffRange != undefined) {
+            var tariffs = el.tariffRange.split('-');
+            if (tariffs[1] > tariffs[0]) {
+                el.tariffRange = tariffs[0].replace('.0', '') + ' - ' +
+                    tariffs[1].replace('.0', '') + '€';
+            } else {
+                el.tariffRange = tariffs[0].replace('.0', '') + '€';
+            }
+            el.priceColor = 'rgb(' + tariffs[0] * 2 + ',' + (200 - (tariffs[0] * 4 ) ) +
+                ',' + tariffs[0] * 4 + ')'
         }
     }
+
+    function filterEventsByTime() {
+        var eventsLenght = $scope.events.length;
+        var maxStartTime = _selStart * 3600000 + new Date().getTime();
+        for (var e = 0; e < eventsLenght; e++) {
+            if ($scope.events[e].startTime > maxStartTime) {
+                $scope.events.splice(e, 1)
+                e = e - 1;
+                eventsLenght = eventsLenght - 1;
+            }
+        }
+    }
+    function getEvents() {
+        filterEventsByTime();
+        EventsFactory.getEvents(_selStart, $rootScope.geoLoc, offset).then(function (events) {
+            events.forEach(colorEvent);
+            updateScope(events, $scope.events, 'eventId');
+        });
+    }
+
+    function getEventsByContaining() {
+        EventsFactory.getEventsByContaining(_research, $rootScope.geoLoc).then(function (events) {
+            events.forEach(colorEvent);
+            updateScope(events, $scope.events, 'eventId');
+        });
+    }
+
+    function getEventsArtistByContaining() {
+        EventsFactory.getArtistsEventsByContaining(_research).then(function (events) {
+            events.forEach(colorEvent);
+            updateScope(events, $scope.events, 'eventId');
+        });
+    }
+
+    function getEventsByGenre() {
+        EventsFactory.getEventsByGenre(_research, offset).then(function (events) {
+            events.forEach(colorEvent);
+            updateScope(events, $scope.events, 'eventId');
+        });
+    }
+
+    function getPlacesEventsByContaining() {
+        EventsFactory.getPlacesEventsByContaining(_research).then(function (events) {
+            events.forEach(colorEvent);
+            updateScope(events, $scope.events, 'eventId');
+        });
+    }
+
+    function getEventsByCity() {
+        EventsFactory.getEventsByCity(_research, offset).then(function (events) {
+            events.forEach(colorEvent);
+            updateScope(events, $scope.events, 'eventId');
+        });
+    }
+
     $scope.moreLimit = function () {
         offset = offset + 20;
         $rootScope.resizeImgHeight();
         getEvents();
     };
-    $scope.getEvents();
-    $rootScope.$watch('geoLoc', function (newval) {
+    $scope.initializeTime = function () {
+        var newName = $rootScope.maxStart;
+        if (newName > 23 && newName <= 38) {
+            newName = (newName - 23) * 24
+        } else if (newName > 38 && newName <= 40) {
+            newName = (newName - 36) * 168;
+        } else if (newName > 40) {
+            newName = (newName - 39) * 720;
+        }
+        var waitForSearchBar = setInterval(function () {
+            if ($rootScope.window == 'small' || $rootScope.window == 'medium') {
+                var textSlider = document.getElementById('timeSearchSliderPhone').getElementsByClassName('md-thumb');
+            } else {
+                var slider = document.getElementsByClassName('bigSlider');
+                var textSlider = [];
+                for (var ii =0; ii < slider.length; ii++) {
+                    textSlider.push(slider[ii].getElementsByClassName('md-thumb')[0]);
+                }
+            }
+            if ($rootScope.path == 'search' || textSlider.length >= 2) {
+                clearInterval(waitForSearchBar);
+                for (var i = 0; i < textSlider.length; i++) {
+                    textSlider[i].innerHTML = '';
+                    textSlider[i].innerHTML = textSlider[i].innerHTML + '<b style="color: #ffffff">' +
+                        $filter('millSecondsToTimeString')(newName) + '</b>';
+                }
+            } else if (_selEvent == false || _research.length > 0) {
+                clearInterval(waitForSearchBar);
+            }
+        }, 100);
+    };
 
-        $scope.getEvents();
+    $scope.initializeTime();
+    getEvents();
+    $rootScope.$watch('geoLoc', function (newval) {
+        if (newval.length > 0) {
+            getEvents();
+        }
     })
 });
