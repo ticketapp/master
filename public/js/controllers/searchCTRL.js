48826d62eb356270c59c03f2d15778a850df8b4e
Loann Delgado <loann.delgado@gmail.com>
2015-04-21 06:42:24 +0200

    cleaned search

--- public/js/controllers/searchCTRL.js
+++ public/js/controllers/searchCTRL.js
@@ -1,7 +1,6 @@
-app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe', '$timeout',
-    'ArtistsFactory', 'EventsFactory', 'OrganizerFactory', 'PlaceFactory',
-    function($rootScope, $http, $scope, $filter, oboe, $timeout, ArtistsFactory, EventsFactory, OrganizerFactory,
-             PlaceFactory){
+app.controller('searchCtrl', ['$scope', '$rootScope', '$filter', '$timeout', 'ArtistsFactory',
+    'EventsFactory', 'OrganizerFactory', 'PlaceFactory',
+    function($rootScope, $scope, $filter, $timeout, ArtistsFactory, EventsFactory, OrganizerFactory, PlaceFactory){
         $scope.limit = 12;
         $scope.artists = [];
         $scope.artistsFb = [];
@@ -186,83 +185,46 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
 
         function getArtistsFacebook() {
             ArtistsFactory.getArtistsFacebookByContaining(_research).then(function (artists) {
-                console.log(artists)
                 updateScope(artists, $scope.artistsFb, 'facebookId', $scope.artists);
                 $scope.loadingFbArt = false;
             });
         }
         function search () {
-            if (_research.length == 0) {
-                if (_selEvent == true) {
-                    getEvents();
-                }
-                if (_selArtist == true) {
-                    getArtists()
-                }
-                if (_selOrganizer == true) {
-                    getOrganizers();
-                }
-                if (_selPlace == true) {
-                    getPlaces();
-                }
-                $scope.artistsFb = [];
-        } else {
             if (_selArtist == true) {
-                if (_research != 'electro' &&
-                    _research != 'reggae' &&
-                    _research != 'rock' &&
-                    _research != 'jazz' &&
-                    _research != 'musique du monde' &&
-                    _research != 'musique latine' &&
-                    _research != 'classique' &&
-                    _research != 'hip-hop' &&
-                    _research != 'chanson' || offset == 0) {
-                    $scope.artistsFb = $filter('filter')($scope.artistsFb, {name: _research});
-                    $scope.artists = $filter('filter')($scope.artists, {name: _research});
+                if (_research.length == 0) {
+                    getArtists()
+                } else {
+                    getArtistsByContaining();
                 }
-                getArtistsByContaining();
             }
             if (_selPlace == true) {
-                $scope.places = $filter('filter')($scope.places, {name :  _research});
-                getPlacesByContaining();
-                getPlacesByCity();
+                if (_research.length == 0) {
+                    getPlaces()
+                } else {
+                    getPlacesByContaining();
+                    getPlacesByCity();
+                }
             }
             if (_selOrganizer == true) {
-                $scope.organizers = $filter('filter')($scope.organizers, {nickname :  _research});
-                getOrganizersByContaining();
+                if (_research.length == 0) {
+                    getOrganizers()
+                } else {
+                    getOrganizersByContaining();
+                }
             }
             if (_selEvent == true) {
-                if (_research != 'electro' &&
-                    _research != 'reggae' &&
-                    _research != 'rock' &&
-                    _research != 'jazz' &&
-                    _research != 'musique du monde' &&
-                    _research != 'musique latine' &&
-                    _research != 'classique' &&
-                    _research != 'hip-hop' &&
-                    _research != 'chanson' || offset == 0) {
-                    $scope.events = $filter('filter')($scope.events, {name: _research})
+                if (_research.length == 0) {
+                    getEvents()
+                } else {
+                    getEventsByContaining();
+                    getEventsArtistByContaining();
+                    getEventsByGenre();
+                    getPlacesEventsByContaining();
+                    getEventsByCity();
                 }
-                getEventsByContaining();
-                getEventsArtistByContaining();
-                getEventsByGenre();
-                getPlacesEventsByContaining();
-                getEventsByCity();
             }
         }
-    }
 
-    function searchArtistFb () {
-        $scope.artistsFb = $filter('filter')($scope.artistsFb, {name :  _research});
-        if ($scope.artistsFb.length < $scope.limit && _selArtist == true && _research.length > 1) {
-            $scope.loadingFbArt = true;
-            getArtistsFacebook();
-        }
-    }
-    search();
-    if (_selArtist == true && _research.length > 2) {
-        searchArtistFb ();
-    }
     $scope.initializeTime = function () {
         var newName = $rootScope.maxStart;
         if (newName > 23 && newName <= 38) {
@@ -293,19 +255,18 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
                 clearInterval(waitForSearchBar);
             }
         }, 100);
-    }
+    };
     $scope.initializeTime();
     $scope.moreLimit = function () {
         offset = offset + 12;
         $scope.limit = $scope.limit + 12;
         $scope.loadingMore = true;
-        $rootScope.resizeImgHeight();
         search();
     };
     var facebookSearch;
     var facebookSearchInterval = 600;
     var otherSearch;
-    var otherSearchInterval = 300;
+    var otherSearchInterval = 400;
     $scope.research = function(newName) {
         if (angular.isDefined(newName)) {
             $scope.loadingMore = true;
@@ -315,21 +276,27 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
             offset = 0;
             if (_research.length == 0) {
                 $scope.initializeTime()
+            } else {
+                $scope.events = $filter('filter')($scope.events, {name: _research});
+                $scope.organizers = $filter('filter')($scope.organizers, {nickname :  _research});
+                $scope.places = $filter('filter')($scope.places, {name :  _research});
+                $scope.artistsFb = $filter('filter')($scope.artistsFb, {name :  _research});
+                $scope.artists = $filter('filter')($scope.artists, {name: _research});
             }
             if (_selArtist == true && _research.length > 2 &&
-                _research != 'electro' &&
-                _research != 'reggae' &&
-                _research != 'rock' &&
-                _research != 'jazz' &&
-                _research != 'musique du monde' &&
-                _research != 'musique latine' &&
-                _research != 'classique' &&
-                _research != 'hip-hop' &&
-                _research != 'chanson'
+                newName != 'electro' &&
+                newName != 'reggae' &&
+                newName != 'rock' &&
+                newName != 'jazz' &&
+                newName != 'musique du monde' &&
+                newName != 'musique latine' &&
+                newName != 'classique' &&
+                newName != 'hip-hop' &&
+                newName != 'chanson'
                 ) {
-                $scope.artistsFb = $filter('filter')($scope.artistsFb, {name :  _research});
+                $scope.loadingFbArt = true;
                 clearTimeout(facebookSearch);
-                facebookSearch = setTimeout(searchArtistFb, facebookSearchInterval);
+                facebookSearch = setTimeout(getArtistsFacebook, facebookSearchInterval);
             }
             clearTimeout(otherSearch);
             otherSearch = setTimeout(search, otherSearchInterval);
@@ -339,12 +306,29 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
     $scope.selArtist = function(newName) {
         if (angular.isDefined(newName)) {
             _selArtist = newName;
-            search();
-            searchArtistFb();
             $scope.limit = 12;
             offset = 0;
             if (newName == true) {
                 $scope.loadingMore = true;
+                if (_research.length == 0) {
+                    getArtists()
+                } else {
+                    getArtistsByContaining();
+                    getArtistsByGenre();
+                    if ( _research.length > 2 &&
+                        newName != 'electro' &&
+                        newName != 'reggae' &&
+                        newName != 'rock' &&
+                        newName != 'jazz' &&
+                        newName != 'musique du monde' &&
+                        newName != 'musique latine' &&
+                        newName != 'classique' &&
+                        newName != 'hip-hop' &&
+                        newName != 'chanson'
+                        ) {
+                        getArtistsFacebook();
+                    }
+                }
             } else {
                 $scope.artists = []
             }
@@ -354,12 +338,20 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
     $scope.selEvent = function(newName) {
         if (angular.isDefined(newName)) {
             _selEvent = newName;
-            search();
             $scope.limit = 12;
             offset = 0;
             if (newName == true) {
                 $scope.loadingMore = true;
-                $scope.initializeTime()
+                if (_research.length == 0) {
+                    $scope.initializeTime()
+                    getEvents()
+                } else {
+                    getEventsByContaining();
+                    getEventsArtistByContaining();
+                    getEventsByCity();
+                    getEventsByGenre();
+                    getPlacesEventsByContaining()
+                }
             } else {
                 $scope.events = []
             }
@@ -369,11 +361,16 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
     $scope.selPlace = function(newName) {
         if (angular.isDefined(newName)) {
             _selPlace = newName;
-            search();
             $scope.limit = 12;
             offset = 0;
             if (newName == true) {
                 $scope.loadingMore = true;
+                if (_research.length == 0) {
+                    getPlaces()
+                } else {
+                    getPlacesByContaining();
+                    getPlacesByCity()
+                }
             } else {
                 $scope.places = []
             }
@@ -388,6 +385,11 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
             offset = 0;
             if (newName == true) {
                 $scope.loadingMore = true;
+                if (_research.length == 0) {
+                    getOrganizers()
+                } else {
+                    getOrganizersByContaining()
+                }
             } else {
                 $scope.organizers = []
             }
@@ -400,6 +402,7 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
         if (angular.isDefined(newName)) {
             $scope.limit = 12;
             offset = 0;
+            $scope.initializeTime();
             if (newName > 23 && newName <= 38) {
                 newName = (newName - 23) * 24
             } else if (newName > 38 && newName <= 40) {
@@ -407,28 +410,23 @@ app.controller('searchCtrl', ['$scope', '$http', '$rootScope', '$filter', 'oboe'
             } else if (newName > 40) {
                 newName = (newName - 39) * 720;
             }
-            if ($rootScope.window == 'small' || $rootScope.window == 'medium') {
-                var textSlider = document.getElementById('timeSearchSliderPhone').getElementsByClassName('md-thumb');
-            } else {
-                var slider = document.getElementsByClassName('bigSlider');
-                var textSlider = [];
-                    for (var ii =0; ii < slider.length; ii++) {
-                        textSlider.push(slider[ii].getElementsByClassName('md-thumb')[0]);
-                    }
-            }
-            for (var i = 0; i < textSlider.length; i++) {
-                textSlider[i].innerHTML = '';
-                textSlider[i].innerHTML = textSlider[i].innerHTML + '<b style="color: #ffffff">' +
-                    $filter('millSecondsToTimeString')(newName) + '</b>';
-            }
             _selStart = newName;
             clearTimeout(StartTimer);
             StartTimer = setTimeout(function () {
-                search();
                 filterEventsByTime();
+                getEvents()
             }, doneStartInterval);
             $scope.loadingMore = true;
             return _selStart;
         }
     };
+    if ($rootScope.geoLoc.length > 0) {
+        search()
+    } else {
+        $rootScope.$watch('geoLoc', function (newVal) {
+            if (newVal.length > 0) {
+                search()
+            }
+        })
+    }
 }]);
