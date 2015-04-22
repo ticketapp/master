320da69b1f5215fa4d00759676c6181e4c368920
Loann Delgado <loann.delgado@gmail.com>
2015-04-22 15:49:26 +0200

    auto follow artists when connected with facebook

--- public/js/controllers/connectCtrl.js
+++ public/js/controllers/connectCtrl.js
@@ -8,31 +8,162 @@ app.controller('connectCtrl', function ($scope, $rootScope, $http, $modal, Artis
 
             })
     };
+    var followErrorCount = 0;
+    var nbAskToFollow = 0;
     function followArtist (id, token) {
-        $http.post('/artists/' + id +'/followByFacebookId').
-            success(function () {
-                console.log(data)
-            }).error(function (data) {
-                if (token != false) {
-                    getArtistPage(id, token)
-                }
-            })
-    }
-    function postArtist (artist) {
-        ArtistsFactory.postArtist(artist.name, artist).then(function (tracks) {
-            $http.post('/artists/' + artist.id +'/followByFacebookId').
-                success(function (data) {
-                    console.log(data);
-                    followArtist(artist.id, false)
-                })
+        nbAskToFollow++;
+        console.log('nbAskToFollow:' + nbAskToFollow);
+        ArtistsFactory.followArtistByFacebookId(id).then(function (isFollowed) {
+            if (isFollowed == 'error') {
+                followErrorCount++;
+                console.log('followErrors:' + followErrorCount);
+                getArtistPage(id, token);
+            }
+
         });
     }
+    var nbArtPost = 0;
+    var nbErrorPost = 0;
+    function postArtist (artist) {
+        if (artist.cover != undefined) {
+            nbArtPost++;
+            console.log('nbArtPost:' + nbArtPost);
+            artist.facebookUrl = artist.link.replace('https://www.facebook.com/', '').replace('pages').replace(/[/]/g, '');
+            artist.facebookId = artist.id;
+            artist.imagePath = artist.cover.source;
+            if (artist.website != undefined) {
+                artist.websites = artist.website;
+            } else {
+                artist.websites ='';
+            }
+            if (artist.bio != undefined) {
+                artist.description = artist.bio;
+            } else {
+                artist.description ='';
+            }
+            if (artist.genre == undefined) {
+                artist.genre = "";
+            }
+            artist.name = artist.username;
+            ArtistsFactory.postArtist(artist.name, artist).then(function (isCreated) {
+                console.log(isCreated);
+                if (isCreated != 'error') {
+                    followArtist(artist.id, false)
+                } else {
+                    nbErrorPost++;
+                    console.log('nbErrorPost:' + nbErrorPost)
+                }
+            });
+        }
+    }
+
+    var nbFbPagesGet = 0;
     function getArtistPage (id, token) {
         $http.get('https://graph.facebook.com/v2.3/' + id + '?access_token=' + token).
             success(function (data) {
                 console.log(data);
+                nbFbPagesGet++;
+                console.log('nbFbPagesGet:' + nbFbPagesGet);
                 postArtist(data)
             })
+    }
+    var nbArtFbPagesFollowedOnFb = 0;
+    function getFbPages (route, token) {
+        $http.get(route).
+            success(function (data) {
+                var pages;
+                var next;
+                var pagesLength;
+                if (data.likes != undefined) {
+                    pages = data.likes.data;
+                    if (data.likes.paging.next != undefined) {
+                        next = data.likes.paging.next
+                    }
+                    pagesLength = data.likes.data.length;
+                } else {
+                    pages = data.data;
+                    if (data.paging.next != undefined) {
+                        next = data.paging.next;
+                    }
+                    pagesLength = data.data.length;
+                }
+                for (var i = 0; i < pagesLength; i++) {
+                    if (pages[i].category == 'Concert tour') {
+                        //folow organizer
+                        //post if not exist
+                    } else if (pages[i].category == "Musician/band") {
+                        //followArtist
+                        nbArtFbPagesFollowedOnFb++;
+                        console.log('nbArtFbPagesFollowedOnFb:' + nbArtFbPagesFollowedOnFb)
+                        followArtist(pages[i].id, token);
+                        // post if not exist
+                    } else if (pages[i].category == "concert venue") {
+                        //folowPlace
+                        // post if not exist
+                    }
+                }
+                if (next == undefined) {
+                    getFbPages(next, token)
+                }
+            })
+    }
+
+    function getUserToken() {
+        $http.get('/users/facebookAccessToken/').
+            success(function (data) {
+                var token = 'CAACEdEose0cBAIyQ2vi7viKASjMb8KoK8PuKs3OdpVWxvVW4oaZAHmkWlsBBkEZBGgclZCXVhh5PfNMxDtIOcjONV0RyNXnRSQPV4hG70VoNeBortntC8wKbTio5bF3aazZC8ummsHPKDGjO5872F3qc2ph9mFg8kWedyj92ZCIwAzpZAP05ZBZAwqZBe458ZC6DpjYHMVIycwcireCZAETsMevOdrJLc9N5moZD'
+                getFbPages('https://graph.facebook.com/v2.3/me?fields=likes&access_token=' + token, token);
+            }).
+            error(function (data) {
+            })
+    }
+
+    function applyLastRequest() {
+        if ($rootScope.lastReq.method == 'post') {
+            if ($rootScope.lastReq.object != "") {
+                $http.post($rootScope.lastReq.path, $rootScope.lastReq.object).
+                    success(function (data) {
+                        $scope.info = $rootScope.lastReq.success;
+                        $rootScope.lastReq = {};
+                    }).
+                    error(function (data) {
+                        if (data.error == 'Credentials required') {
+                            $rootScope.storeLastReq('post', $rootScope.lastReq.path, $rootScope.lastReq.object, $rootScope.lastReq.success)
+                        } else {
+
+                            $scope.info = 'Désolé une erreur s\'est produite';
+                        }
+                    })
+            } else {
+                $http.post($rootScope.lastReq.path).
+                    success(function (data) {
+                        $scope.info = $rootScope.lastReq.success;
+                        $rootScope.lastReq = {};
+                    }).
+                    error(function (data) {
+                        if (data.error == 'Credentials required') {
+                            $rootScope.storeLastReq('post', $rootScope.lastReq.path, $rootScope.lastReq.object, $rootScope.lastReq.success)
+                        } else {
+                            $scope.info = 'Désolé une erreur s\'est produite';
+                        }
+                    })
+            }
+        }
+    }
+
+    function getConnected (connectWin) {
+        if (connectWin.document.getElementById('top') != undefined || connectWin.document.getElementById('top') != 'null') {
+            if (connectWin.document.getElementById('top').getAttribute("ng-init") == '$root.connected = true') {
+                $rootScope.passConnectedToTrue();
+                connectWin.close();
+                getUserToken();
+                if ($rootScope.lastReq != {}) {
+                    applyLastRequest();
+                }
+            }
+        } else {
+            getConnected();
+        }
     }
     $scope.connectLink = function (url) {
         var connectWin = window.open(url, "", "toolbar=no, scrollbars=no, resizable=no, width=500, height=500");
@@ -41,94 +172,8 @@ app.controller('connectCtrl', function ($scope, $rootScope, $http, $modal, Artis
                 clearInterval(changePath)
             }
             if (connectWin.location.href == 'http://localhost:9000/#/connected') {
-                clearInterval(changePath)
-                function getConnected () {
-                    if (connectWin.document.getElementById('top') != undefined || connectWin.document.getElementById('top') != 'null') {
-                        if (connectWin.document.getElementById('top').getAttribute("ng-init") == '$root.connected = true') {
-                            $rootScope.passConnectedToTrue();
-                            connectWin.close();
-                            function getFbPages (route, token) {
-                                $http.get(route + token).
-                                    success(function (data) {
-                                        console.log(data)
-                                        if (data.likes != undefined) {
-                                            var pages = data.likes.data;
-                                            var next = data.likes.paging.next
-                                            var pagesLength = data.likes.data.length;
-                                        } else {
-                                            var pages = data.data;
-                                            var next = data.paging.next;
-                                            var pagesLength = data.data.length;
-                                        }
-                                        for (var i = 0; i < pagesLength; i++) {
-                                            if (pages[i].category == 'Concert tour') {
-                                                //folow organizer
-                                                //post if not exist
-                                            } else if (pages[i].category == "Musician/band") {
-                                                //followArtist
-                                                var pageId = pages[i].id;
-                                                followArtist(pageId, token);
-                                                // post if not exist
-                                            } else if (pages[i].category == "concert venue") {
-                                                //folowPlace
-                                                // post if not exist
-                                            }
-                                        }
-                                        getFbPages(next)
-                                    })
-                            }
-                            $http.get('/users/facebookAccessToken/').
-                                success(function (data) {
-                                    console.log(data)
-                                    var token = 'CAACEdEose0cBAB2QWdAlF1PeOxPCHGXrg7X1SYrQaqOaClyZB6DVH9mYBVYSyBFjmq1Xf3paRIO0wmCsBPzP56lwSoQGFwm9BSmY8lI3PREoz2kmuBchT0h8RKXNg6x3qEsfZC5wwtWXDYWuq72e5JDJP9PRqCcnkY5nJvu3E3ZA9IXgaiGiBgNAuwylZC2cBgxf3c3gUPmLMkRUYzs6DKiMc5fflZBMZD'
-                                    getFbPages('https://graph.facebook.com/v2.3/me?fields=likes&access_token=', token);
-
-                                    /*$http.get('https://graph.facebook.com/v2.3/me/accounts?access_token=CAACEdEose0cBAABPUdlSmjwteEoQYLwNoFvGOAOGpozBkuRbERMDZB92FTuMTeTvNTLX9ZBGPRAVIvdssK8ntDcAe3cXqKsZBfQZC1dQmEZABIwKnhoT8uiNJZCCU6WrjZC3VLRFoX4KJXEwwo0tLa4Sqa5ajpVxLhFRkRZBS5KYf2NFbnHHgk0JkcZAqNk9knPCwSohvvO6iM25kjljedltPiDgGXAaNp3sZD').
-                                        success(function (data) {
-                                            console.log(data)
-                                        })*/
-
-                                }).
-                                error(function (data) {
-                                })
-                            if ($rootScope.lastReq != {}) {
-                                if ($rootScope.lastReq.method == 'post') {
-                                    if ($rootScope.lastReq.object != "") {
-                                        $http.post($rootScope.lastReq.path, $rootScope.lastReq.object).
-                                            success(function (data) {
-                                                $scope.info = $rootScope.lastReq.success;
-                                                $rootScope.lastReq = {};
-                                            }).
-                                            error(function (data) {
-                                                if (data.error == 'Credentials required') {
-                                                    $rootScope.storeLastReq('post', $rootScope.lastReq.path, $rootScope.lastReq.object, $rootScope.lastReq.success)
-                                                } else {
-
-                                                    $scope.info = 'Désolé une erreur s\'est produite';
-                                                }
-                                            })
-                                    } else {
-                                        $http.post($rootScope.lastReq.path).
-                                            success(function (data) {
-                                                $scope.info = $rootScope.lastReq.success;
-                                                $rootScope.lastReq = {};
-                                            }).
-                                            error(function (data) {
-                                                if (data.error == 'Credentials required') {
-                                                    $rootScope.storeLastReq('post', $rootScope.lastReq.path, $rootScope.lastReq.object, $rootScope.lastReq.success)
-                                                } else {
-                                                    $scope.info = 'Désolé une erreur s\'est produite';
-                                                }
-                                            })
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        getConnected();
-                    }
-                }
-                getConnected();
+                clearInterval(changePath);
+                getConnected(connectWin);
             }
         }, 1000);
     };
--- public/js/services/artiste.js
+++ public/js/services/artiste.js
@@ -66,6 +66,8 @@ app.factory('ArtistsFactory', function ($http, $q, oboe) {
             if(factory.artists == true){
                 deferred.resolve(factory.artists);
             } else {
+                console.log(artist)
+                console.log(searchPattern)
                 oboe.post('artists/createArtist', {
                     searchPattern: searchPattern,
                     artist: {
@@ -79,26 +81,43 @@ app.factory('ArtistsFactory', function ($http, $q, oboe) {
                     }
                 }).start(function (data, etc) {
                 })
-                    .done(function (value) {
-                        function saveTrack (track) {
-                            if (track.redirectUrl == undefined) {
-                                track.redirectUrl = track.url;
-                            }
-                            $http.post('/tracks/create', {
-                                artistFacebookUrl: artist.facebookUrl,
-                                redirectUrl : track.redirectUrl,
-                                title: track.title,
-                                url: track.url,
-                                platform: track.platform,
-                                thumbnailUrl: track.thumbnailUrl
-                            }).error(function (data) {
-                            })
+                .done(function (value) {
+                    console.log('posted')
+                    function saveTrack (track) {
+                        if (track.redirectUrl == undefined) {
+                            track.redirectUrl = track.url;
                         }
-                        value.forEach(saveTrack);
-                        deferred.resolve(value);
+                        $http.post('/tracks/create', {
+                            artistFacebookUrl: artist.facebookUrl,
+                            redirectUrl : track.redirectUrl,
+                            title: track.title,
+                            url: track.url,
+                            platform: track.platform,
+                            thumbnailUrl: track.thumbnailUrl
+                        }).error(function (data) {
+                        })
+                    }
+                    value.forEach(saveTrack);
+                    deferred.resolve(value);
+                })
+                .fail(function (error) {
+                    deferred.resolve('error');
+                    console.log(error)
+                });
+            }
+            return deferred.promise;
+        },
+        followArtistByFacebookId : function (id) {
+            var deferred = $q.defer();
+            if(factory.artists == true){
+                deferred.resolve(factory.artists);
+            } else {
+                $http.post('/artists/' + id +'/followByFacebookId').
+                    success(function (data) {
+                        deferred.resolve(data);
+                    }).error(function (data) {
+                        deferred.resolve('error');
                     })
-                    .fail(function (error) {
-                    });
             }
             return deferred.promise;
         }
