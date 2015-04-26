888b3a823b7eb0c616e59740d43bf9a095f78ced
Loann Delgado <loann.delgado@gmail.com>
2015-04-27 00:09:37 +0200

    error follow artists

--- public/js/services/user.js
+++ public/js/services/user.js
@@ -1,14 +1,14 @@
 app.factory ('UserFactory', function ($http, $q){
     var factory = {
         user : false,
-        getToken : function(){
+        getToken : function () {
             var deferred = $q.defer();
             if(factory.user == true){
                 deferred.resolve(factory.user);
             } else {
                 $http.get('/users/facebookAccessToken/')
                 .success(function(data, status){
-                    factory.user = data;
+                    factory.user = 'CAACEdEose0cBAFRShP5EVANSQCJR3a6uOs97TdHbWFCQd2iVh3Kbx1rfsAOFFwigb6YBncPW0Mqt1Kj4r1C25wdGTGpZAN2bn1QwqnmAQi7TEPdy7pBYDPZBdINVjZChv5FN3K5cWcBZAofTNRBIPdolhHXOMZCj1xTcu9Abvq37IcmUNLhwdZA9xGABEuLj7Vwv7KU0p9pVJZBqXccS48ZClK30EYaldcwZD';
                     deferred.resolve(factory.user);
                 }).error(function(data, status){
                     deferred.reject('erreur');
@@ -16,7 +16,7 @@ app.factory ('UserFactory', function ($http, $q){
             }
         return deferred.promise;
         },
-        deletePlaylist : function(id){
+        deletePlaylist : function(id) {
             var deferred = $q.defer();
             if(factory.user == true){
                 deferred.resolve(factory.user);
