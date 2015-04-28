4b09b884c32503eae771c701dfb9651bd880d3db
Loann Delgado <loann.delgado@gmail.com>
2015-04-28 22:44:36 +0200

    get token

--- public/js/services/user.js
+++ public/js/services/user.js
@@ -8,7 +8,7 @@ app.factory ('UserFactory', function ($http, $q){
             } else {
                 $http.get('/users/facebookAccessToken/')
                 .success(function(data, status){
-                    factory.user = 'CAACEdEose0cBAFRShP5EVANSQCJR3a6uOs97TdHbWFCQd2iVh3Kbx1rfsAOFFwigb6YBncPW0Mqt1Kj4r1C25wdGTGpZAN2bn1QwqnmAQi7TEPdy7pBYDPZBdINVjZChv5FN3K5cWcBZAofTNRBIPdolhHXOMZCj1xTcu9Abvq37IcmUNLhwdZA9xGABEuLj7Vwv7KU0p9pVJZBqXccS48ZClK30EYaldcwZD';
+                    factory.user = data;
                     deferred.resolve(factory.user);
                 }).error(function(data, status){
                     deferred.reject('erreur');
