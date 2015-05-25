fb9e2ebcbce6663dedbf3792aa95d2a7992678c4
Loann Delgado <loann.delgado@gmail.com>
2015-05-25 22:12:50 +0200

    fixed some css bugs

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -48,7 +48,7 @@ CREATE TABLE infos (
   animationStyle            VARCHAR
 );
 
-INSERT INTO infos (title, content) VALUES ('Timeline', 's - 4 avant la bêta :) :)');
+INSERT INTO infos (title, content) VALUES ('Timeline', 's - 3 avant la bêta :) :)');
 INSERT INTO infos (title, content) VALUES ('Bienvenue', 'Jetez un oeil, ça vaut le détour');
 INSERT INTO infos (title, content) VALUES (':) :) :)', 'Déjà deux utilisateurs !!!');
 INSERT INTO infos (title, content) VALUES ('TicketApp', 'Cest simple, cest beau, ça fuse');
