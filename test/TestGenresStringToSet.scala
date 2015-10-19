62adaa0c7f89126f7ab55ee2cad1fdb19e0e2099
SiloSas <loann@debian>
2015-10-19 12:54:12 +0200

    TestGenresStringToSet ok

--- test/TestGenresStringToSet.scala
+++ test/TestGenresStringToSet.scala
@@ -1,17 +1,30 @@
-/*
-import models.Genre
+import models.{ArtistMethods, TrackMethods, GenreMethods, Genre}
 import org.scalatest.Matchers._
 import org.scalatest._
 import org.scalatestplus.play._
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.inject.guice.GuiceApplicationBuilder
+import services.{SearchYoutubeTracks, SearchSoundCloudTracks, Utilities}
 
 class TestGenresStringToSet extends PlaySpec with OneAppPerSuite {
 
+  val appBuilder = new GuiceApplicationBuilder()
+  val injector = appBuilder.injector()
+  val dbConfProvider = injector.instanceOf[DatabaseConfigProvider]
+  val utilities = new Utilities()
+  val genreMethods = new GenreMethods(dbConfProvider, utilities)
+  val trackMethods = new TrackMethods(dbConfProvider, utilities)
+  val searchSoundCloudTracks = new SearchSoundCloudTracks(dbConfProvider, utilities, trackMethods, genreMethods)
+  val searchYoutubeTracks = new SearchYoutubeTracks(dbConfProvider, genreMethods, utilities, trackMethods)
+  val artistMethods = new ArtistMethods(dbConfProvider, genreMethods, searchSoundCloudTracks, searchYoutubeTracks,
+    trackMethods, utilities)
+
   "A sequence of genres as a string" must {
 
-    /*"return an empty set for None" in {
+    "return an empty set for None" in {
       val genres = List(None, None)
 
-      val genresSets: List[Set[Genre]] = genres.map { genresStringToGenresSet }
+      val genresSets: List[Set[Genre]] = genres.map { genreMethods.genresStringToGenresSet }
 
       val expectedResult = List(Set.empty, Set.empty)
 
@@ -21,7 +34,7 @@ class TestGenresStringToSet extends PlaySpec with OneAppPerSuite {
     "return a unique low case genre for a single word without punctuation" in {
       val genres = List(Some("rock"), Some("Rap"))
 
-      val genresSets: List[Set[Genre]] = genres.map { genresStringToGenresSet }
+      val genresSets: List[Set[Genre]] = genres.map { genreMethods.genresStringToGenresSet }
 
       val expectedResult = List(Set(Genre(None, "rock")), Set(Genre(None, "rap")))
 
@@ -31,7 +44,7 @@ class TestGenresStringToSet extends PlaySpec with OneAppPerSuite {
     "return genres split by comas in the string given" in {
       val genres = List(Some("Rock, rockstep"), Some("Hi-tech soul, Cosmic jazz-funk, Riot disco, Timeless electro"))
 
-      val genresSets: List[Set[Genre]] = genres.map { genresStringToGenresSet }
+      val genresSets: List[Set[Genre]] = genres.map { genreMethods.genresStringToGenresSet }
 
       val expectedResult = List(Set(Genre(None, "rock"), Genre(None, "rockstep")), Set(Genre(None, "hi-tech soul"),
         Genre(None, "cosmic jazz-funk"), Genre(None, "riot disco"), Genre(None, "timeless electro"))
@@ -43,7 +56,7 @@ class TestGenresStringToSet extends PlaySpec with OneAppPerSuite {
     "return genres split by spaces in the string given" in {
       val genres = List(Some("Rock Pop Covers"), Some("Hi-tech soul jazz-funk"))
 
-      val genresSets: List[Set[Genre]] = genres.map { genresStringToGenresSet }
+      val genresSets: List[Set[Genre]] = genres.map { genreMethods.genresStringToGenresSet }
 
       val expectedResult = List(Set(Genre(None, "rock"), Genre(None, "pop"), Genre(None, "covers")),
         Set(Genre(None, "hi-tech"), Genre(None, "soul"), Genre(None, "jazz-funk"))
@@ -55,12 +68,12 @@ class TestGenresStringToSet extends PlaySpec with OneAppPerSuite {
     "return return hip-hop" in {
       val genres = List(Some("Hip-Hop"), Some("Indie Pop-Folk"))
 
-      val genresSets: List[Set[Genre]] = genres.map { genresStringToGenresSet }
+      val genresSets: List[Set[Genre]] = genres.map { genreMethods.genresStringToGenresSet }
 
       val expectedResult = List(Set(Genre(None, "hip-hop")), Set(Genre(None, "indie"), Genre(None, "pop-folk")))
 
       genresSets should contain theSameElementsAs expectedResult
-    }*/
+    }
   }
 }
 /*
@@ -91,4 +104,4 @@ class TestGenresStringToSet extends PlaySpec with OneAppPerSuite {
    Some("Soirée fetichiste"),
    Some("""UK Garage, House N Garage, Bass Music, Jackin House, ("2 Step, 4 to the floor/4x4")  and Bassline from the past, present and future..."""))
 */
-*/
+
