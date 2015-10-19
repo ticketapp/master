76f406b150f48d18971fc33b15d1c1a736141c57
SiloSas <loann@debian>
2015-10-19 11:13:24 +0200

    test artistModel ok (waiting for silouette & all artists relations)

--- test/TestArtistModel.scala
+++ test/TestArtistModel.scala
@@ -1,132 +1,151 @@
-/*
+import java.util.UUID
 
-import javax.inject.Inject
-import javax.swing.text.Utilities
-
-
-import models.{Artist, Track}
+import models._
 import org.postgresql.util.PSQLException
 import org.scalatest.concurrent.ScalaFutures._
 import org.scalatest.time.{Seconds, Span}
 import org.scalatestplus.play._
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.iteratee.Iteratee
+import services.{SearchYoutubeTracks, SearchSoundCloudTracks, Utilities}
 
 import scala.util.{Failure, Success}
 
 class TestArtistModel extends PlaySpec with OneAppPerSuite {
 
-  /*val utilities = new Utilities()
-  val organiserMethods
-  val addressMethods = new AddressMethods(DatabaseConfigProvider)
-  val trackMethods = new TrackMethods(DatabaseConfigProvider, utilities)
-  val searchSoundCloudTracks = new SearchSoundCloudTracks(DatabaseConfigProvider, utilities)
-  val genreMethods = new GenreMethods(DatabaseConfigProvider, utilities)
-  val artistMethods = new ArtistMethods(DatabaseConfigProvider, genreMethods, )*/
+  val appBuilder = new GuiceApplicationBuilder()
+  val injector = appBuilder.injector()
+  val dbConfProvider = injector.instanceOf[DatabaseConfigProvider]
+  val utilities = new Utilities
+  val trackMethods = new TrackMethods(dbConfProvider, utilities)
+  val genreMethods = new GenreMethods(dbConfProvider, utilities)
+  val searchSoundCloudTracks = new SearchSoundCloudTracks(dbConfProvider, utilities, trackMethods, genreMethods)
+  val searchYoutubeTrack = new SearchYoutubeTracks(dbConfProvider, genreMethods, utilities, trackMethods)
+  val artistMethods = new ArtistMethods(dbConfProvider, genreMethods, searchSoundCloudTracks, searchYoutubeTrack,
+    trackMethods, utilities)
 
   "An Artist" must {
 
-    /*"be saved and deleted in database and return the new id" in {
+    "be saved and deleted in database and return the new id" in {
       val artist = Artist(None, Option("facebookIdTestArtistModel"), "artistTest", Option("imagePath"),
         Option("description"), "facebookUrl", Set("website"))
-      val artistId = Artist.save(artist).get
-      try {
-        find(artistId) mustBe Option(artist.copy(id = Some(artistId),
-          description = Some("<div class='column large-12'>description</div>")))
-        delete(artistId) mustBe 1
-      } finally {
-        delete(artistId)
+      whenReady(artistMethods.save(artist), timeout(Span(5, Seconds))) { savedArtist =>
+        try {
+          whenReady(artistMethods.find(savedArtist.id.get), timeout(Span(5, Seconds))) { foundArtist =>
+            foundArtist mustBe Option(artist.copy(id = Some(savedArtist.id.get),
+              description = Some("<div class='column large-12'>description</div>")))
+            whenReady(artistMethods.delete(savedArtist.id.get), timeout(Span(5, Seconds))) { _ mustBe 1 }
+          }
+        } finally {
+          artistMethods.delete(savedArtist.id.get)
+        }
       }
     }
 
-    "be followed and unfollowed by a user" in {
+    /*"be followed and unfollowed by a user" in {
       val artist = Artist(None, Option("facebookId3"), "artistTest3", Option("imagePath"), Option("description"),
         "facebookUrl3", Set("website"))
-      val artistId = Artist.save(artist).get
-
-      try {
-        followByArtistId("userTestId", artistId)
-        isFollowed(IdentityId("userTestId", "oauth2"), artistId) mustBe true
-        unfollowByArtistId("userTestId", artistId) mustBe Success(1)
-      } finally {
-        Artist.delete(artistId)
+      whenReady(artistMethods.save(artist), timeout(Span(5, Seconds))) { savedArtist =>
+        try {
+          whenReady(artistMethods.followByArtistId(artistMethods.UserArtistRelation("userTestId", savedArtist.id.get)),
+            timeout(Span(5, Seconds))) {
+            whenReady(artistMethods.isFollowed(IdentityId("userTestId", "oauth2"), savedArtist.id.get)) { isFollowed =>
+              isFollowed mustBe true
+              whenReady(artistMethods.unfollowByArtistId(artistMethods.UserArtistRelation("userTestId",
+                savedArtist.id.get))) { unfollow =>
+                unfollow mustBe Success(1)
+              }
+            }
+          }
+        } finally {
+          artistMethods.delete(savedArtist.id.get)
+        }
       }
     }
 
     "not be followed twice" in {
       val artist = Artist(None, Option("facebookId3"), "artistTest3", Option("imagePath"), Option("description"),
         "facebookUrl3", Set("website"))
-      val artistId = Artist.save(artist).get
-
-      try {
-        followByArtistId("userTestId", artistId)
-        followByArtistId("userTestId", artistId) match {
-          case Failure(psqlException: PSQLException) => psqlException.getSQLState mustBe UNIQUE_VIOLATION
-          case _ => throw new Exception("follow twice an artist worked !")
+      whenReady(artistMethods.save(artist), timeout(Span(5, Seconds))) { savedArtist =>
+        try {
+          whenReady(artistMethods.followByArtistId(artistMethods.UserArtistRelation("userTestId", savedArtist.id.get)),
+          timeout(Span(5, Seconds))) {
+            whenReady(artistMethods.followByArtistId(artistMethods.UserArtistRelation("userTestId", savedArtist.id.get)),
+              timeout(Span(5, Seconds))) {
+              case Failure(psqlException: PSQLException) => psqlException.getSQLState mustBe UNIQUE_VIOLATION
+              case _ => throw new Exception("follow twice an artist worked !")
+            }
+          }
+        } finally {
+          whenReady(artistMethods.unfollowByArtistId(artistMethods.UserArtistRelation("userTestId",
+            savedArtist.id.get))) { unfollow =>
+            unfollow mustBe Success(1)
+            artistMethods.delete(savedArtist.id.get)
+          }
         }
-      } finally {
-        unfollowByArtistId("userTestId", artistId)
-        Artist.delete(artistId)
       }
-    }
+    }*/
 
     "be updated" in {
       val artist = Artist(None, Option("facebookId3"), "artistTest3", Option("imagePath"), Option("description"),
         "facebookUrl3", Set("website"))
-      val artistId = Artist.save(artist)
-
-      try {
-        val updatedArtist = artist.copy(id = artistId, name = "updatedName")
-        update(updatedArtist)
-
-        find(artistId.get) mustBe Option(updatedArtist)
-      } finally {
-        Artist.delete(artistId.get)
+      whenReady(artistMethods.save(artist), timeout(Span(5, Seconds))) { savedArtist =>
+        try {
+          val updatedArtist = artist.copy(id = Option(savedArtist.id.get), name = "updatedName")
+          artistMethods.update(updatedArtist)
+          whenReady(artistMethods.find(savedArtist.id.get), timeout(Span(5, Seconds))) { _ mustBe Option(updatedArtist) }
+        } finally {
+          artistMethods.delete(savedArtist.id.get)
+        }
       }
     }
 
     "have his websites updated" in {
       val artist = Artist(None, Option("facebookId2"), "artistTest2", Option("imagePath"), Option("description"),
         "facebookUrl2", Set("website"))
-      val artistId = Artist.save(artist)
-
-      try {
-        addWebsite(artistId, "normalizedUrl")
-
-        find(artistId.get) mustBe Option(artist.copy(id = artistId, websites = Set("website", "normalizedUrl"),
-          description = Some("<div class='column large-12'>description</div>")))
-
-      } finally {
-        delete(artistId.get)
+      whenReady(artistMethods.save(artist), timeout(Span(5, Seconds))) { savedArtist =>
+        try {
+          whenReady(artistMethods.addWebsite(savedArtist.id.get, "normalizedUrl"), timeout(Span(5, Seconds))) { resp =>
+            whenReady(artistMethods.find(savedArtist.id.get), timeout(Span(5, Seconds))) { foundArtist =>
+              foundArtist mustBe Option(artist.copy(id = Option(savedArtist.id.get), websites = Set("website", "normalizedUrl"),
+                description = Some("<div class='column large-12'>description</div>")))
+            }
+          }
+        } finally {
+          artistMethods.delete(savedArtist.id.get)
+        }
       }
     }
 
     "have another website" in {
       val artist = Artist(None, Option("facebookId3"), "artistTest3", Option("imagePath"), Option("description"),
         "facebookUrl3", Set("website"))
-      val maybeTrack = Option(Track(randomUUID, "title", "url", 'S', "thumbnailUrl", "artistFacebookUrl", "artistName",
+      val maybeTrack = Option(Track(UUID.randomUUID, "title", "url", 'S', "thumbnailUrl", "artistFacebookUrl", "artistName",
         Option("redirectUrl")))
-      val artistId = Artist.save(artist)
-      val artistWithId = artist.copy(id = artistId)
-
-      addSoundCloudWebsiteIfMissing(maybeTrack, artistWithId)
-
-      try {
-        find(artistId.get) mustBe Option(artistWithId.copy(websites = Set("website", "redirecturl"),
-          description = Some("<div class='column large-12'>description</div>")))
-      } catch {
-        case e:Exception => throw e
-      } finally {
-        delete(artistId.get) mustBe 1
+      whenReady(artistMethods.save(artist), timeout(Span(5, Seconds))) { savedArtist =>
+        val artistWithId = artist.copy(id = Option(savedArtist.id.get))
+        artistMethods.addSoundCloudWebsiteIfMissing(maybeTrack, artistWithId)
+        try {
+          whenReady(artistMethods.find(savedArtist.id.get), timeout(Span(5, Seconds))) { artistFound =>
+            artistFound mustBe Option(artistWithId.copy(websites = Set("website", "redirecturl"),
+              description = Some("<div class='column large-12'>description</div>")))
+          }
+        } catch {
+          case e: Exception => throw e
+        } finally {
+          whenReady(artistMethods.delete(savedArtist.id.get), timeout(Span(5, Seconds))) { _ mustBe 1 }
+        }
       }
     }
 
-    "get tracks for an artist" in {
+    /*"get tracks for an artist" in {
       val patternAndArtist = PatternAndArtist("Feu! Chatterton",
         Artist(Some(236),Some("197919830269754"),"Feu! Chatterton", None ,None , "kjlk",
           Set("soundcloud.com/feu-chatterton", "facebook.com/feu.chatterton", "twitter.com/feuchatterton",
             "youtube.com/user/feuchatterton", "https://www.youtube.com/channel/UCGWpjrgMylyGVRIKQdazrPA"),
           List(),List(),None,None))
-      val enumerateTracks = getArtistTracks(patternAndArtist)
+      val enumerateTracks = artistMethods.getArtistTracks(patternAndArtist)
       val iteratee = Iteratee.foreach[Set[Track]]{track => println("track = " + track)}
 
       whenReady(enumerateTracks |>> iteratee, timeout(Span(6, Seconds))) { a=>
@@ -135,4 +154,3 @@ class TestArtistModel extends PlaySpec with OneAppPerSuite {
     }*/
   }
 }
-*/
