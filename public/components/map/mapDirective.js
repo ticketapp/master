d7a494a4e3de2c7570eff9e13be8704d868112ac
SiloSas <loann@debian>
2015-11-13 22:25:11 +0100

    mobile home ok

--- public/components/map/mapDirective.js
+++ public/components/map/mapDirective.js
@@ -1,22 +1,22 @@
 angular.module('claudeApp')
-    .directive('mapControls', function(){
-    return{
-        restrict : 'E',
-        templateUrl:'assets/components/map/mapControls.html'
-    }
-});
+    .directive('mapControls', function() {
+        return {
+            restrict : 'E',
+            templateUrl:'assets/components/map/mapControls.html'
+        }
+    });
 
 angular.module('claudeApp')
-    .directive('mapContainer', function(){
-        return{
+    .directive('mapContainer', function() {
+        return {
             restrict : 'C',
             controller:'MapCtrl'
         }
     });
 
 angular.module('claudeApp')
-    .directive('stylizedMap', function(){
-        return{
+    .directive('stylizedMap', function() {
+        return {
             restrict : 'E',
             templateUrl:'assets/components/map/map.html'
         }
