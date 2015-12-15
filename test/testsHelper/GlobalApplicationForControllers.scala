4a198048582885357cae9a8fb6ba43a7a9e160e1
Simon Garnier <simongarnier07@hotmail.fr>
2015-12-15 19:16:48 +0100

    Correct (WIP4) 2.sql inconsistent states bugs

--- test/testsHelper/GlobalApplicationForControllers.scala
+++ test/testsHelper/GlobalApplicationForControllers.scala
@@ -14,7 +14,8 @@ import play.api.test.PlaySpecification
 import scala.concurrent._
 
 
-trait GlobalApplicationForControllers extends PlaySpecification with Mockito with BeforeAll with AfterAll with Context with Injectors {
+trait GlobalApplicationForControllers
+    extends PlaySpecification with Mockito with BeforeAll with AfterAll with Context with Injectors {
 
   val defaultUserUUID =  UUID.fromString("077f3ea6-2272-4457-a47e-9e9111108e44")
 
