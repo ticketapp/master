1c1a45ed9291d01eeb35dc041d826f1822aa7233
Simon Garnier <simongarnier07@hotmail.fr>
2016-02-03 21:28:41 +0100

    Refactor happening class

--- client/src/main/scala/events/Happening.scala
+++ client/src/main/scala/events/Happening.scala
@@ -8,20 +8,22 @@ import places.PlaceWithAddress
 
 import scala.scalajs.js.Date
 import scala.scalajs.js.annotation.JSExportAll
+
+
 @JSExportAll
 case class Happening(id: Option[Long] ,
-                  facebookId: Option[String] ,
-                  isPublic: Boolean,
-                  isActive: Boolean,
-                  name: String,
-                  geographicPoint: Geometry,
-                  description: Option[String] ,
-                  startTime: Date,
-                  endTime: Option[Date] ,
-                  ageRestriction: Int = 16,
-                  tariffRange: Option[String] ,
-                  ticketSellers: Option[String] ,
-                  imagePath: Option[String] )
+                     facebookId: Option[String] ,
+                     isPublic: Boolean,
+                     isActive: Boolean,
+                     name: String,
+                     geographicPoint: Geometry,
+                     description: Option[String] ,
+                     startTime: Date,
+                     endTime: Option[Date] ,
+                     ageRestriction: Int = 16,
+                     tariffRange: Option[String] ,
+                     ticketSellers: Option[String] ,
+                     imagePath: Option[String] )
 
 case class Geometry(point: String)
 
