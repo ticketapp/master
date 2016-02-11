ff02eac3eea7775d92bda4ded5311738131dcfc1
Simon Garnier <simongarnier07@hotmail.fr>
2016-02-11 04:08:52 +0100

    Replace Option[String] by String and remove a useless line

--- server/app/services/LoggerHelper.scala
+++ server/app/services/LoggerHelper.scala
@@ -4,12 +4,9 @@ import play.api.Logger
 
 trait LoggerHelper {
 
-  def log(maybeMessage: Option[String] = None, maybeException: Option[Exception] = None)
+  def log(message: String = "", maybeException: Option[Exception] = None)
          (implicit line: sourcecode.Line, file: sourcecode.File) = {
     val className = file.value.drop(file.value.lastIndexOf("/") + 1).stripSuffix(".scala")
-    val message = maybeMessage.getOrElse("")
-
-    Logger("access").logger.error("accessLog")
 
     maybeException match {
       case Some(e) => Logger.logger.error(className + ":" + line.value + message + ": ", e)
