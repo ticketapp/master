63d480d9e8a9f003dc1a4662fc615449c809d759
Simon Garnier <simongarnier07@hotmail.fr>
2016-02-13 22:33:01 +0100

    Modify english texts

--- server/public/scripts/services/translateConfig.js
+++ server/public/scripts/services/translateConfig.js
@@ -1,14 +1,14 @@
-angular.module('angularTranslateApp', ['pascalprecht.translate', 'ngSanitize']).config(function($translateProvider) {
-    $translateProvider.translations('en', {
-        HEADLINE: 'Sell your tickets',
-        INTRO_TEXT: "be payed know, claude is the buyer"
+angular.module("angularTranslateApp", ["pascalprecht.translate", "ngSanitize"]).config(function($translateProvider) {
+    $translateProvider.translations("en", {
+        HEADLINE: "Sell your concert tickets",
+        INTRO_TEXT: "Get paid now, regardless of whether a buyer is found or not"
     })
-    .translations('fr', {
-        HEADLINE: 'Vendez nous votre billet de concert',
+    .translations("fr", {
+        HEADLINE: "Vendez nous votre billet de concert",
         INTRO_TEXT: "Récuperez instantanément votre argent, sans vous soucier qu'un acheteur soit trouvé"
     });
     var userLang = (navigator.language || navigator.userLanguage).substring(0, 2);
     $translateProvider.preferredLanguage(userLang);
-    $translateProvider.useSanitizeValueStrategy('sanitize');
+    $translateProvider.useSanitizeValueStrategy("sanitize");
     //All documentation: https://angularjs.de/artikel/angularjs-i18n-ng-translate
 });
