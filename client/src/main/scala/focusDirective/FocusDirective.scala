e92f84d193dec1b882907522e2b056d8d4c3178a
Simon Garnier <simongarnier07@hotmail.fr>
2016-02-21 00:28:42 +0100

    Correct some style

--- client/src/main/scala/focusDirective/FocusDirective.scala
+++ client/src/main/scala/focusDirective/FocusDirective.scala
@@ -1,6 +1,5 @@
 package focusDirective
 
-
 import com.greencatsoft.angularjs.core.Timeout
 import com.greencatsoft.angularjs._
 import org.scalajs.dom.Element
@@ -13,9 +12,9 @@ import scala.scalajs.js.annotation.JSExport
 class FocusDirective(timeout: Timeout) extends AttributeDirective {
 
   override def link(scopeType: ScopeType, elements: Seq[Element], attributes: Attributes): Unit = {
-    elements.map{_.asInstanceOf[Html]}.foreach { element =>
+    elements.map(_.asInstanceOf[Html]).foreach { element =>
       attributes.$observe("focus", (value: String) => {
-        if(value.toBoolean) {
+        if (value.toBoolean) {
           element match {
             case _: Input => element.focus()
             case _ => element.getElementsByTagName("input").item(0).asInstanceOf[Html].focus()
