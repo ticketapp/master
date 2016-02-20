d86ff861317847dc07db5c626a6c8c30661939d5
Simon Garnier <simongarnier07@hotmail.fr>
2016-02-20 23:04:06 +0100

    Improve logger in order to authorize throwable

--- server/app/services/LoggerHelper.scala
+++ server/app/services/LoggerHelper.scala
@@ -4,13 +4,19 @@ import play.api.Logger
 
 trait LoggerHelper {
 
-  def log(message: String = "", maybeException: Option[Exception] = None)
+  def log(message: String = "", maybeException: Option[Exception] = None, maybeThrowable: Option[Throwable] = None)
          (implicit line: sourcecode.Line, file: sourcecode.File) = {
     val className = file.value.drop(file.value.lastIndexOf("/") + 1).stripSuffix(".scala")
 
-    maybeException match {
-      case Some(e) => Logger.logger.error(className + ":" + line.value + message + ": ", e)
-      case _ => Logger.logger.info(className + ":" + line.value + " " + message)
+    maybeThrowable match {
+      case Some(t) =>
+        Logger.logger.error(className + ":" + line.value + ": " + message + ": ", t)
+
+      case _ =>
+        maybeException match {
+          case Some(e) => Logger.logger.error(className + ":" + line.value + ": " + message + ": ", e)
+          case _ => Logger.logger.info(className + ":" + line.value  + ": " + message)
+        }
     }
   }
 }
