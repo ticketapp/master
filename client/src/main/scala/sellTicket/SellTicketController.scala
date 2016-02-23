e602c3e918b57ad64a0076482636b1cb999351e0
Simon Garnier <simongarnier07@hotmail.fr>
2016-02-23 01:07:01 +0100

    Correct SellTicketController bug

--- client/src/main/scala/sellTicket/SellTicketController.scala
+++ client/src/main/scala/sellTicket/SellTicketController.scala
@@ -1,30 +1,29 @@
 package sellTicket
 
 import admin.AdminRoutes
-import com.greencatsoft.angularjs.core.{RouteParams, Scope, Timeout}
+import com.greencatsoft.angularjs.core.{RouteParams, Timeout}
 import com.greencatsoft.angularjs.{AbstractController, injectable}
 import events.EventsRoutes
-import geolocation.GeolocationService
 import httpServiceFactory.HttpGeneralService
 import utilities.jsonHelper
+
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.scalajs.js
 import scala.scalajs.js.JSON
 import scala.scalajs.js.annotation.JSExportAll
 
 @JSExportAll
 @injectable("sellTicketController")
-class SellTicketController(sellTicketScope: SellTicketScope, service: HttpGeneralService, timeout: Timeout,
-                       geolocationService: GeolocationService, routeParams: RouteParams)
-  extends AbstractController[SellTicketScope](sellTicketScope) with jsonHelper {
-
+class SellTicketController(sellTicketScope: SellTicketScope, httpService: HttpGeneralService, timeout: Timeout,
+                           routeParams: RouteParams)
+    extends AbstractController[SellTicketScope](sellTicketScope) with jsonHelper {
 
   val eventId = routeParams.get("eventId").get.toString.toInt
-  
-  service.get(AdminRoutes.findTariffsByEventId(eventId)) map { tariffsFound =>
+
+  httpService.get(AdminRoutes.findTariffsByEventId(eventId)) map { tariffsFound =>
     timeout(() => sellTicketScope.tariffs = JSON.parse(tariffsFound))
   }
-  service.get(EventsRoutes.find(eventId)) map { foundEvent =>
+
+  httpService.get(EventsRoutes.findById(eventId)) map { foundEvent =>
     timeout(() => sellTicketScope.event = JSON.parse(foundEvent))
   }
 }
